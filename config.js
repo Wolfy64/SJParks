0f866f3f2b01f15eec786cbc488db82e9eeb3c4b
Irina <irishka2863@yahoo.com>
2018-09-01 11:06:21 -0700

    Edited configuration variables. Need mongodb url to connect mongoose

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-require('dotenv-safe').load();
-
 const cfg = {};
 
 // HTTP Port to run our web application
-cfg.port = process.env.PORT || 3000;
+cfg.port = process.env.PORT || 1337;
 
 // A random string that will help generate secure one-time passwords and
 // HTTP sessions
@@ -15,13 +13,13 @@ cfg.secret = process.env.APP_SECRET || 'keyboard cat';
 // A good practice is to store these string values as system environment
 // variables, and load them from there as we are doing below. Alternately,
 // you could hard code these values here as strings.
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+cfg.accountSid = 'ACd241712f0dddcf05da280dd2c87d79c4';
+cfg.authToken = 'e588e3f2721f174ae344679510ad68ae';
 
 // A Twilio number you control - choose one from:
 // https://www.twilio.com/user/account/phone-numbers/incoming
 // Specify in E.164 format, e.g. "+16519998877"
-cfg.twilioNumber = process.env.TWILIO_NUMBER;
+cfg.twilioNumber = '+18317774596';
 
 // MongoDB connection string - MONGO_URL is for local dev,
 // MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
