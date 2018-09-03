06ffe7e35ab4bbb0c49fea8c8a56aa3d35099b6e
unknown <apele001@fiu.edu>
2018-09-03 15:12:03 -0700

    message.js

--- test/lib/messageSender.test.js
+++ test/lib/messageSender.test.js
@@ -12,6 +12,7 @@ describe('subscriber', function() {
   let messageSender = {};
 
   before(() => {
+    console.log('wtf?');
     return connectionHelper.connect().then(() => {
       messageSender = proxyquire('../../lib/messageSender', {
         'twilio': () => {
@@ -28,11 +29,11 @@ describe('subscriber', function() {
   it('sends a message to subscribers', function() {
     // given
     const subscribers = [
-      new Subscriber({phone: 'phone1', subscribed: true}),
-      new Subscriber({phone: 'phone2', subscribed: true})];
+      new Subscriber({phone: '+17864261786', subscribed: true}),
+      new Subscriber({phone: '+17863167718', subscribed: true})];
 
     createStub.returns(Promise.resolve('message'));
-
+    console.log(subscribers);
     // when
     return messageSender.sendMessageToSubscribers(subscribers, 'message', 'url')
       .then(() => {
