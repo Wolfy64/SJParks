6fe165b3c76534ffc0d6d25f19dd5a376ec70ec4
Henry O Zorrilla <hernyz37@gmail.com>
2018-10-03 02:10:12 -0700

    log in validation and log out button

--- controllers/login.js
+++ controllers/login.js
@@ -1,51 +1,18 @@
 // var bcrypt = require('bcrypt');
-var crypto = require('crypto')
-
-const user = require('../models/User');
-
-//register: storing name, email and password and redirecting to home page after signup
-exports.newUser = function (request, response) {
-
-    // bcrypt.hash(request.body.psw + request.body.uname, saltRounds, function (err, hash) {
-    //     user.findOne({
-    //         Username: request.body.uname,
-    //         Password: hash,
-    //         admin: request.body.isAdmin
-    //         },  { runValidators: true, context: 'query' }, function (err, user) {
-    //         //  I'm not sure exactly what happens if it finds NO users are found, as such I will assume a "NULL"might be returned
-    //         if (err || !user) {
-    //             user.create({
-    //                 username: request.body.usname,
-    //                 password: hash,
-    //                 admin: request.body.isAdmin,
-    //                 notes: request.body.eNts
-    //             }).then(() => {
-    //                 console.log('PASS: created new user')
-    //             //  TODO not sure what to do in here
-    //             });
-    //             return response.redirect('/admin');
-    //         }
-    //     });
-    // });
-}
+const crypto = require('crypto');
+const User = require('../models/User');
 
 //  authorize the login information
 exports.validate = function (request, response) {
 
-    // bcrypt.hash(request.body.psw + request.body.uname, saltRounds, function (err, hash) {
-
-    //     bcrypt.compare(request.body.psw + request.body.uname, hash, function(err, res) {
-    //         // res == true
-    //     });
-
-    //     user.findOneAndUpdate({ Username: request.body.uname, Password: hash},
-    //         { runValidators: true, context: 'query' }, 
-    //         function (err, user) {
-    //         return err || !user.admin ? response.redirect('/login') : response.redirect('/user');
-    //     });
-    // });
-
-}
+    const usrnm = request.body.uname;
+    
+    User.findOne({
+        Username: usrnm
+    }, function (err, user) {
+        if(user.validPassword(request.body.psw) && user.username === usrnm || user.valid  ){
+        return err || !user.admin ? response.redirect('/') : response.redirect('/admin');}
+    });
 
 exports.requireLogin = function (req, res, next) {
     console.log('#####################################################');
--- models/User.js
+++ models/User.js
@@ -6,26 +6,27 @@ var secret = require('../config').secret;
 const Schema = mongoose.Schema;
 
 const UserSchema = new mongoose.Schema({
-
-    username: {type: String, lowercase: true,  unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'invalid username'], index: true},
-
-    password: {type: String, required: [true, "Password Required"], index: true},
-
-    admin: {type: Boolean, default: false},
+    
+    salt: String,
 
     first_name: {type: String, required: true, max: 100},
 
     last_name: {type: String, required: true, max: 100},
 
-    email: {type: String, lowercase: true,  unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true}, 
-    
-    phone: {type: String, default: "(999) 999 - 9999", unique: true} ,
-    
-    notes:{type: String, default: "Nothing to note"},
+    username: {type: String, lowercase: true,  unique: true, match: [/^[a-zA-Z0-9]+$/, 'invalid username'], index: true},
 
-    parks:[{ type: Schema.Types.ObjectId, ref: 'Park' }],
+    password: {type: String, required: [true, "Password Required"], index: true},
 
-    issues:[{ type: Schema.Types.ObjectId, ref: 'Message' }]
+    admin: {type: Boolean, default: false},
+
+    email: {type: String, lowercase: true, required: [true, "you must enter an email"], unique: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid'], index: true}, 
+    
+    phone: {type: String, match: [ /(+\n+)+\n+\-\n+/, "(999) 999 - 9999"], required: [true, "you must enter a phone number"], unique: true},
+
+    parks:[{ type: Schema.Types.ObjectId, ref: 'Park' }]
+    //for easy referene to issues
+    //issues:[{ type: Schema.Types.ObjectId, ref: 'Issue' }]
 
 }, {timestamps: true});
 
@@ -37,14 +38,16 @@ UserSchema.virtual('name').get(function () { return this.last_name + ', ' + this
 // }   );
 
 //schema methods
+// sets the password with a hash
 UserSchema.methods.setPassword = function(password){
     this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    this.password = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
   };
 
+//validate password
 UserSchema.methods.validPassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-    return this.hash === hash;
+    return this.password === hash;
 }
 
 // add 'Unique' validation to this schema
