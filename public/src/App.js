6cd574234199360b2abb3ed570e1d80c7c20ba3c
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-10-20 12:00:15 -0700

    added react components claudiobardales

--- public/src/App.js
+++ public/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+class updatePreview extends React.Component{
+  render (){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <p>{this.props.name}</p>
+          <p>{this.props.time}</p>
+        </div>
+        <p>{this.props.update}</p>
+        <a href="#">See More</a>
       </div>
     );
   }
+};
+
+function SeeFullHistory(props){
+  render (){
+    return <button>{this.props.name}</button>;
+  };
 }
+
+function title(props){
+  render () {
+    return (
+      <div>
+        <h1>Latest Updates</h1>
+      </div>
+    );
+  }
+};
+
+function textupdate(props){
+  render (){
+    return (
+        <button>{this.props.name}</button>
+    );
+  }
+}
+
+
 
 export default App;
