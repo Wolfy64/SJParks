5a03edfa07f7a7497ef61347b31207bc22e51069
Irina Mityugova <irishka2863@yahoo.com>
2018-10-27 16:19:21 -0700

    Navbar nav-buttons

--- client/src/components/sidenav.js
+++ client/src/components/sidenav.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NavButton from './navbutton';
 
 export default class SideNav extends Component {
   render() {
     return (
-      <div id="wrapper row">
-      <div id="sidebar-wrapper col-md-2">
-            <h1>SJParks</h1>
+      <div className="full-height">
+      <div className="col-sm-3 d-flex flex-column">
+            <h1 className="text-center">SJParks</h1>
             <p className="text-center">Admin</p>
             <ul className="navbar-nav">
                 <li className="sidebar-brand">
-                <li><navButton name="Updates" action="updatePage"/></li>
-                <li><navButton name="Parks" action="parkPage"/></li>
-                <li><navButton name="Users" action="userPage" /></li>
-                <li><navButton name="Logout" action="logoutPage" /></li>
+                <li><NavButton name="Updates" action="updatePage"/></li>
+                <li><NavButton name="Parks" action="parkPage"/></li>
+                <li><NavButton name="Users" action="userPage" /></li>
+                <li><NavButton className="align-self-end" name="Logout" action="logoutPage" /></li>
                 </li>
             </ul>
         </div>
