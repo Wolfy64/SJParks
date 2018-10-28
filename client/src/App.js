be4963704170fbfd9507227cace87acd3bbf87de
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-10-27 19:27:51 -0700

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import SideNav from './components/sidenav';
+import TopNav from './components/topnav';
 
 class App extends React.Component{
   render (){
     return (
-        <div>
+      <div>
+        <div className="d-flex">
         <SideNav />
+        <TopNav />
         </div>
+      </div>
     );
   }
 };
