4b06350ead909e6d9f74857f4502d81558ea882a
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-11-02 15:22:44 -0700

    Added Forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import SideNav from './components/sidenav';
-import TopNav from './components/topnav';
 
 class App extends React.Component{
   render (){
     return (
       <div>
-        <div className="d-flex">
-        <SideNav />
-        <TopNav />
+        <div className="mx-auto">
+            <h1>SJParks</h1>
+            <div class="form-group">
+              <label for="username">User ID: </label> <br />
+              <input type="name" name="name" />
+            </div>
+            <div className ="form-group">
+            <label for="password">Password: </label> <br />
+            <input type="password" placeholder="Password" />
+            </div>
         </div>
       </div>
     );
