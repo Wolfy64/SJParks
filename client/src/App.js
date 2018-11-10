7428f009306aab08d9bb276ee625f0d7896ed5d9
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-11-09 16:22:29 -0800

    added alert for functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login'
 import Sidenav from './components/sidenav'
 import Topnav from './components/topnav'
 
--- client/src/components/topnav.js
+++ client/src/components/topnav.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './topnav.css';
 
+
 class TopNav extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    page: "users",
+  };
+
+  this.toUsers = this.toUsers.bind(this);
+  }
+
+  toUsers(e){
+    alert("This buttons goes to users");
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="top-nav">
         <nav className="navbar">
-            <button>newUpdate</button>
-            <img></img>
+            <li>newUpdate</li>
+            <li><a href ='' onClick={this.toUsers}><img></img>IMAGE</a></li>
         </nav>
       </div>
     );
