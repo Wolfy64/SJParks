0d2a014f0f72fb11b627bcd595f9bf0261bc6cdf
Irina Mityugova <irishka2863@yahoo.com>
2018-11-10 13:09:42 -0800

    Edit 1 - avoiding 3rd party to search

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,50 +1,37 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Suggestions from 'components/Suggestions'
-
-const { API_KEY } = process.env
-const API_URL = 'http://api.musicgraph.com/api/v2/artist/suggest'
+import Parklabels from 'components/parklabels'
 
 class Search extends Component {
-  state = {
-    query: '',
-    results: []
-  }
-
-  getInfo = () => {
-    axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
-      .then(({ data }) => {
-        this.setState({
-          results: data.data
-        })
-      })
-  }
-
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-          this.getInfo()
+  constructor() {
+        super();
+        this.state = {
+            searchText: '',
+            searchResults: []
         }
-      } else if (!this.state.query) {
-      }
-    })
-  }
+    }
 
-  render() {
-    return (
-      <form>
-        <input
-          placeholder="Search for..."
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-        />
-        <Suggestions results={this.state.results} />
+    onChange(e) {
+        this.setState({searchText: e.target.value});
+    }
+
+    getResults() {
+        calltodb(searchText).then(e => {
+            this.setState({searchResults: e.value})
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                  placeholder="Search for..."
+                  ref={input => this.search = input}
+                  onChange={this.handleInputChange} />
+            <Parklabels results={this.state.results} />
       </form>
-    )
-  }
+        )
+    }
 }
 
 export default Search
