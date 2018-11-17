c4df790c683ddd2e91698012f80e4172675732db
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-11-16 20:57:21 -0800

    Added filtercontainer.js

--- client/src/components/parks.js
+++ client/src/components/parks.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 export default class parks extends Component {
+  constructor(props){
+    super(props);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.state = {
+      parkFilter: ''
+    }
+  }
+
+  handleFilter = (e) =>{
+    this.setState({
+      parkFilter: e.target.value
+    })
+    this.props.onChange(event.target.value) 
+  }
+
   render() {
     return (
       <div>
         <h1>Parks</h1>
         <div class="form-group">
-            <label for="username">New Park's Title: </label> <br />
+            <label for="username">New Parks Title: </label> <br />
             <input type="name" name="name" placeholder="Parks" />
         </div>
         <div class="form-group">
@@ -14,7 +29,16 @@ export default class parks extends Component {
             <input type="name" name="name" placeholder="Keyword" />
         </div>
         <button>Create New Park</button>
+        <input type="name" id="filter" 
+        value= {this.state.parkFilter}
+        onChange={this.handleFilter}
+        placeholder="search park by name"/>
       </div>
     )
   }
 }
+
+
+
+
+
