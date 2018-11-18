c534a4779c7f0df1a0338f9dfa20b6847ec156e8
Claudio Bardales <claudiobardales@Claudios-Macbook.local>
2018-11-17 20:17:28 -0800

    Added Comments

--- client/src/components/parks.js
+++ client/src/components/parks.js
@@ -60,45 +60,47 @@ export default class parks extends Component {
 }
 
 
+/*
+export default class parks extends Component {
+  constructor(props){
+    super(props);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.state = {
+      parkFilter: ''
+    }
+  }
 
-// export default class parks extends Component {
-//   constructor(props){
-//     super(props);
-//     this.handleFilter = this.handleFilter.bind(this);
-//     this.state = {
-//       parkFilter: ''
-//     }
-//   }
+  handleFilter = (e) => {
+    this.setState({
+      parkFilter: e.target.value
+    })
+    this.props.onChange(e.target.value) 
+  }
 
-//   handleFilter = (e) => {
-//     this.setState({
-//       parkFilter: e.target.value
-//     })
-//     this.props.onChange(e.target.value) 
-//   }
+  render() {
+    return (
+      <div>
+        <h1>Parks</h1>
+        <div className="form-group">
+            <label htmlFor="username">New Parks Title: </label> <br />
+            <input type="name" name="name" placeholder="Parks" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="username">Keyword </label> <br />
+            <input type="name" name="name" placeholder="Keyword" />
+        </div>
+        <button>Create New Park</button>
+        <input type="name" id="filter" 
+        value= {this.state.parkFilter}
+        onChange={this.handleFilter}
+        placeholder="search park by name"/>
+      </div>
+    )
+  }
+}
+*/
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Parks</h1>
-//         <div className="form-group">
-//             <label htmlFor="username">New Parks Title: </label> <br />
-//             <input type="name" name="name" placeholder="Parks" />
-//         </div>
-//         <div className="form-group">
-//             <label htmlFor="username">Keyword </label> <br />
-//             <input type="name" name="name" placeholder="Keyword" />
-//         </div>
-//         <button>Create New Park</button>
-//         <input type="name" id="filter" 
-//         value= {this.state.parkFilter}
-//         onChange={this.handleFilter}
-//         placeholder="search park by name"/>
-//       </div>
-//     )
-//   }
-// }
-
+// FILTER CODE!!! //
 
 
 
