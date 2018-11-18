2b643f6def4b5ef825d484a0b6651de957ea6a54
Irina Mityugova <irishka2863@yahoo.com>
2018-11-17 20:56:31 -0800

    load more button headache

--- client/src/components/historypost.js
+++ client/src/components/historypost.js
@@ -1,44 +1,72 @@
-import React, { Component } from 'react'
-let data = [
-    {name:'Jeff Tomson',
-    date: "11/28/18",
-    time: "11:20",
-    parkIDs: ['ROSE', 'ROSE'],
-    message: 'Bramhall Park and Rose Garden, \nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'},
-    
-    {name:'Peterson Laderhavin',
-    date: Date.now(),
-    time: "11:20",
-    parkIDs: ['ROSE'],
-    message: 'Bramhall Park and Rose Garden, \nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'},
-    
-    {name:'Fernando Bordalex',
-    date: Date.now(),
-    time: "11:20",
-    parkIDs: ['ROSE'],
-    message: 'Bramhall Park and Rose Garden,\nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'}
-    ]
-    
+import React, { Component } from 'react'   
 
 export default class Post extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      loadMore: false
+    }
+    this.users = [
+        {name:'Jeff Tomson',
+        date: "11/28/18",
+        time: "11:20",
+        parkIDs: ['ROthgSE', 'ROzdfSE', 'ROSzdE', 'ROzddSE', 'DFzndgROSE', 'ROzdfSE'],
+        message: 'Bramhall Park and Rose Garden, \nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'},
+
+        {name:'Peterson Laderhavin',
+        date: Date.now(),
+        time: "11:20",
+        parkIDs: ['ROSE'],
+        message: 'Bramhall Park and Rose Garden, \nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'},
+
+        {name:'Fernando Bordalex',
+        date: Date.now(),
+        time: "11:20",
+        parkIDs: ['ROSE'],
+        message: 'Bramhall Park and Rose Garden,\nDear resident, we recommend not visiting Rose Garden or Bramhall Park due to a fire in the Almaden area. We will update you as soon as it is safe to visit the park.'}
+    ]
+    }
+
+  showMore = () =>{
+    this.state.loadMore ? this.setState({loadMore: false}) : this.setState({loadMore: true})
+  }      
+      
   render (){
     return (
-      <div>{data.map((e, index)=>
-          <div className="card" key={index}>
-        <div className="dark-blue white-text">
-            <i>Arrow</i>
-            <p className="white-text">{e.name}</p>
-            <p className="white-text">{e.date} at {e.time}</p>
+        <div>
+            {this.users.map((user, index)=>
+            <div className="card" key={index}>
+
+                <div className="dark-blue white-text">
+                    <i>Arrow</i>
+                    <p className="white-text">{user.name}</p>
+                    <p className="white-text">{user.date} at {user.time}</p>
+                </div>
+
+                <div>
+                    {user.parkIDs.map((park, index)=>
+                        <p key={index}>{park}</p>
+                    )}
+                </div>
+
+                <div className="gray bottom-round">
+                    <button onClick={this.showMore}
+                     className="blue-link">
+                        {this.state.loadMore ? (
+                        <span>Show less</span>
+                        ) : (
+                        <span>{user.parkIDs.length>3 ? (user.parkIDs.length-3).toString()+" more.." : null}</span>
+                        )
+                      }
+                        
+                    </button>
+                </div>
+
+                <p>{user.message}</p>
+
+            </div>     
+            )}
         </div>
-        <div>{e.parkIDs.map((park)=><p>{park}</p>)}</div>
-        <div className="gray bottom-round">
-            <a className="blue-link">11 more...</a>
-        </div>
-        <p>{e.message}</p>
-      </div>
-          
-          
-          )}</div>
       )
   }
-};
+}
