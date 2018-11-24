0622bd900dac6b9dd0a773f464e77cbcacf2cb2d
Irina Mityugova <irishka2863@yahoo.com>
2018-11-24 12:39:34 -0800

    Filter functionality

--- client/src/components/parks.js
+++ client/src/components/parks.js
@@ -3,43 +3,46 @@ import Todo from './todo';
 import Items from './items';
 
 export default class parks extends Component {
-  inputElement = React.createRef(); // createRef? 
-  constructor(props){ // and you can tell here that i added props to the component.
-    super(props); // It it didn't have any before. 
-    this.handleFilter = this.handleFilter.bind(this)
-    this.state = {
-      items: [],
-      currentItem: {
-        text: '',
-        key: '',
-      },
-      parkFilter: [],
-    }
-  }
-    handleInput = e => {
-      const itemText = e.target.value; // Still Kinda Fuzzy About this Concept
-      const currentItem = {text: itemText, key:Date.now() } // key is a key value pair from currentItem
-      this.setState({
-        currentItem,
-        
-      })
+    inputElement = React.createRef(); // createRef? 
+    constructor(props){ // and you can tell here that i added props to the component.
+        super(props); // It it didn't have any before. 
+        this.handleFilter = this.handleFilter.bind(this)
+        this.state = {
+        items: [],
+        parkFilter: [],
+        filter: '',
+        newPark: '',
+        }
     }
 
-     addItem = e => {
-       e.preventDefault();
-      const newItem = this.state.currentItem
-      if (newItem.text !== ''){ // .text? is it a string method? if it is, what does it do?
-        console.log(newItem)
-        const items = [...this.state.items, newItem] // ... -> This is a Spread Operator but idk i don't understand the concept fully
+    handleInput = e => { //generic handleInput handles the change for any input field using the name and the value properties.
+        const {name, value} = e.target;
+        console.log('name',name)
+        console.log('value',value)
         this.setState({
-          items: items,
-          currentItem: {text: '', key: ''},
-        })  
-      }
+            [name]: value
+        });
+    }
+
+    addItem = e => {
+        e.preventDefault();
+        const { newPark } = this.state;
+        const newItem = {
+            key: Date.now(), //needs to be another input from the user - a parkID
+            text: newPark,
+        }
+        if (newItem.text !== ''){ // .text is a name for the park that came from user's input
+            console.log(newItem)
+            const items = [...this.state.items, newItem] //concatinates new item object to an array of parks
+            this.setState({
+                items: items,
+                newPark: '',
+            })  
+        }
     }
 
      deleteItem = key => {
-      const filteredItems = this.state.items.filter(item => { // i forgot what filter() does i need to reasearch this again. 
+      const filteredItems = this.state.items.filter(item => { //filter() takes in an array and returns a new array with each item that passed through the true/false function.
         return item.key !== key;
       })
       this.setState({
@@ -47,34 +50,41 @@ export default class parks extends Component {
       })
     }
 
-    handleFilter = (e) => {
+    handleFilter = e => {
       e.preventDefault();  
-      const filtered = this.state.items.filter(item=> item.text.includes('hi')); //needs to filter through items according to the inputed value instead of 'hi'
+      const filtered = this.state.items.filter(item=> {
+          return item.text.includes(this.state.filter)
+      }); //needs to filter through items according to the inputed value instead of 'hi'
       this.setState({
         parkFilter: filtered
       })
     }
 
-     render(){
-      return(
-        <div>
-          <h2>List</h2>
-          <Todo
-            addItem={this.addItem}
-            inputElement={this.inputElement}
-            handleInput={this.handleInput}
-            currentItem={this.state.currentItem}
-          />
-          <Items entries={this.state.items} deleteItem={this.deleteItem}/>
-          
-          <h2>Filter</h2>
-          <form onSubmit={this.handleFilter}>
-            <input value={this.state.text} type="text" id="filter" placeholder="Search Parks.."/>
-            <button type="submit">Search</button>
-          </form>
-          <Items entries={this.state.parkFilter}/>
-        </div>
-      )
+    render(){
+        console.log('STATE', this.state)
+        return(
+            <div>
+                <h2>List</h2>
+                <Todo
+                    addItem={this.addItem}
+                    inputElement={this.inputElement}
+                    handleInput={this.handleInput}
+                    newPark={this.state.newPark}
+                />
+                <Items entries={this.state.items} deleteItem={this.deleteItem}/>
+
+                <h2>Filter</h2>
+                <form onSubmit={this.handleFilter}>
+                    <input 
+                        name='filter'
+                        value={this.state.filter} 
+                        onChange={this.handleInput} 
+                        type="text" id="filter" placeholder="Search Parks.."/>
+                    <button type="submit">Search</button>
+                </form>
+                <Items entries={this.state.parkFilter} deleteItem={this.deleteItem}/>
+            </div>
+        )
     }
 }
 
--- client/src/components/todo.js
+++ client/src/components/todo.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
-    componentDidUpdate(){
-        this.props.inputElement.current.focus();
-    }
   render() {
     return (
       <div className='todoListMain'>
         <div className ='header'>
         <form onSubmit={this.props.addItem}>
         <input 
+            name='newPark'
             placeholder="New Park..."
-            ref={this.props.inputElement}
-            value={this.props.currentItem.text}
+            value={this.props.newPark}
             onChange={this.props.handleInput}
             />
             <button type="submit">Add Park</button>        
