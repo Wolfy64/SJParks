9f73e7931499e09409489c82ced70ec70c27ebd1
Irina Mityugova <irishka2863@yahoo.com>
2018-11-30 14:54:26 -0800

    Folding posts

--- client/src/components/historypost.js
+++ client/src/components/historypost.js
@@ -5,28 +5,31 @@ export default class Post extends React.Component{
     super()
     this.state = {
       loadMore: false,
+      isOpen: false,
     }
     }
 
   showMore = () =>{
     this.state.loadMore ? this.setState({loadMore: false}) : this.setState({loadMore: true})
-  }      
+  }  
+  open = () =>{
+    this.state.isOpen ? this.setState({isOpen: false}) : this.setState({isOpen: true})
+  }     
       
   render (){
     return (
         <div>
-            <div className="card">
-
-                <div className="dark-blue white-text">
-                    <i className="fas fa-caret-right text-primary"></i>
-
+        {this.state.isOpen ? (
+            <div>
+                <div onClick={this.open} className="dark-blue white-text">
+                        <i className="fas fa-caret-down text-primary"></i>
                     <p className="white-text">{this.props.post.name}</p>
                     <p className="white-text">{this.props.post.date} at {this.props.post.time}</p>
                 </div>
 
                 <div className="gray bottom-round">
                 
-                        {this.props.post.parkIDs.length<3 ? ( //if length >3
+                        {this.props.post.parkIDs.length<3 ? ( 
                             <div>
                             {this.props.post.parkIDs.map((parkID, index)=> //show all parkID's and no button
                                 <p key={index}>{parkID}</p>
@@ -34,7 +37,7 @@ export default class Post extends React.Component{
                             </div>
                         ) : ( 
                             <div>
-                            {this.state.loadMore ? ( //else check loadMore, if true - show all & button 
+                            {this.state.loadMore ? ( //if length >3 check loadMore === true - show all & button 
                                 <div>
                                     {this.props.post.parkIDs.map((parkID, index)=> //show all parkID's and no button
                                         <p key={index}>{parkID}</p>
@@ -54,18 +57,24 @@ export default class Post extends React.Component{
                                     </button>
                                 </div>
                             )}
-                            </div>
-                            
-                            
+                            </div> 
                         )
-                      }
-                
+                        }
                 </div>
 
                 <p>{this.props.post.message}</p>
-
             </div>
+        ) : ( //if the section isOpen === false
+            <div>
+                <div onClick={this.open} className="dark-blue white-text">
+                        <i className="fas fa-caret-right text-primary"></i>
+                    <p className="white-text">{this.props.post.name}</p>
+                    <p className="white-text">{this.props.post.date} at {this.props.post.time}</p>
+                </div>
+            </div>
+        )
+        }
         </div>
-      )
+    )
   }
 }
