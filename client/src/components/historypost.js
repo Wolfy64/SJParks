173f82967fb752a142c13b599abc3c031f7df6a7
Irina Mityugova <irishka2863@yahoo.com>
2018-11-30 13:52:03 -0800

    Show More function +++

--- client/src/components/historypost.js
+++ client/src/components/historypost.js
@@ -24,22 +24,42 @@ export default class Post extends React.Component{
                     <p className="white-text">{this.props.post.date} at {this.props.post.time}</p>
                 </div>
 
-                <div>
-                    {this.props.post.parkIDs.map((parkID, index)=>
-                        <p key={index}>{parkID}</p>
-                    )}
-                </div>
-
                 <div className="gray bottom-round">
-                <button onClick={this.showMore}
-                     className="blue-link">
-                        {this.state.loadMore ? (
-                        <span>Show less</span>
-                        ) : (
-                        <span>{this.props.post.parkIDs.length>3 ? (this.props.post.parkIDs.length-3).toString()+" more.." : null}</span>
+                
+                        {this.props.post.parkIDs.length<3 ? ( //if length >3
+                            <div>
+                            {this.props.post.parkIDs.map((parkID, index)=> //show all parkID's and no button
+                                <p key={index}>{parkID}</p>
+                            )}
+                            </div>
+                        ) : ( 
+                            <div>
+                            {this.state.loadMore ? ( //else check loadMore, if true - show all & button 
+                                <div>
+                                    {this.props.post.parkIDs.map((parkID, index)=> //show all parkID's and no button
+                                        <p key={index}>{parkID}</p>
+                                    )}
+                                    <button onClick={this.showMore}
+                                    className="blue-link">Show less</button>
+                                </div>
+                                
+                            ) : (
+                                <div>
+                                    {this.props.post.parkIDs.slice(0, 3).map((parkID, index)=> //show all parkID's and no button
+                                        <p key={index}>{parkID}</p>
+                                    )}
+                                    <button onClick={this.showMore}
+                                    className="blue-link">{this.props.post.parkIDs.length>3 ? (
+                                        this.props.post.parkIDs.length-3).toString()+" more.." : null}
+                                    </button>
+                                </div>
+                            )}
+                            </div>
+                            
+                            
                         )
                       }
-                </button>
+                
                 </div>
 
                 <p>{this.props.post.message}</p>
