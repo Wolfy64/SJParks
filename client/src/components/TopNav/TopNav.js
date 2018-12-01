047b98a1f7fa8e5f3be6e5e25b8f46b99c9e6229
David De Wulf <contact.dewulf@gmail.com>
2018-12-01 00:20:27 -0800

    Refactor Code

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import './topnav.css';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import {Users} from 'react';
+import { Link } from 'react-router-dom';
 
-
-class TopNav extends React.Component {
-  constructor(props){
-  super(props);
-  this.state = {
-    page: "users",
-  };
-
-  this.toUsers = this.toUsers.bind(this);
-  }
-
-  toUsers(e){
-    alert("This buttons goes to users");
-    e.preventDefault();
-  }
-  render() {
-    return (
-      <Router>
-      <div className="top-nav">
-        <nav className="navbar">
-            <li><Link to="/users" onClick={this.toUsers}><img></img>IMAGE</Link></li>
-        </nav>
-      </div>
-      </Router>
-    );
-  }
-}
+const TopNav = () => (
+  <div className='top-nav'>
+    <nav className='navbar'>
+      <li>
+        <Link to='/profile'>IMAGE</Link>
+      </li>
+    </nav>
+  </div>
+);
 
 export default TopNav;
-
