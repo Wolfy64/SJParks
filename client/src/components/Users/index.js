cfb0bca94fa42b8d0d235f9ddfed168fc36e600f
David De Wulf <contact.dewulf@gmail.com>
2018-12-05 10:35:30 -0800

    Change Component from stateful to stateless

--- client/src/components/Users/index.js
+++ client/src/components/Users/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import UsersForm from './UsersForm';
 import UsersList from './UsersList';
 
-class Users extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Users Page</h1>
-        <UsersForm />
-        <UsersList />
-      </div>
-    );
-  }
-}
+const Users = () => (
+  <>
+    <h1>Users Page</h1>
+    <UsersForm />
+    <UsersList />
+  </>
+);
 
 export default Users;
