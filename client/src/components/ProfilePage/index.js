84bce1dc68adfe9d093f3adbe594b36ce8033102
David De Wulf <contact.dewulf@gmail.com>
2018-12-05 10:59:04 -0800

    Add index.js

---
+++ client/src/components/ProfilePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const ProfilePage = () => (
+  <>
+    <h1>Profile Page</h1>
+    <UserForm />
+  </>
+);
+
+export default ProfilePage;
