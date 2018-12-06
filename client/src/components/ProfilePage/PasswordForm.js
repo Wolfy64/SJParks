b6ba5ebe95fd1a50586c18c0db7eeb25373534a2
David De Wulf <contact.dewulf@gmail.com>
2018-12-05 21:01:57 -0800

    Split Component in smaller one

--- client/src/components/ProfilePage/PasswordForm.js
+++ client/src/components/ProfilePage/PasswordForm.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Input from '../UI/Form/Input';
 
 const initialState = {
-  fullName: '',
-  email: '',
-  phone: '',
+  currentPassword: '',
+  newPassword: '',
+  repeatPassword: '',
   showError: false
 };
 
-class UserForm extends React.Component {
+class PasswordForm extends React.Component {
   state = initialState;
 
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -17,23 +17,22 @@ class UserForm extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
     const hasError = this.hasError();
-    const { email, fullName, phone } = this.state;
-    const data = { email, fullName, phone };
+    const { currentPassword, newPassword } = this.state;
 
     // Check if the form has error
-    const dataIsEmpty = Object.values(data).includes('');
+    const passIsEqual = currentPassword === newPassword;
 
-    if (hasError || dataIsEmpty) {
+    if (hasError || !newPassword || !passIsEqual) {
       this.setState({ showError: true });
     }
 
-    if (!hasError && !dataIsEmpty) {
-      const payload = { method: 'POST', body: JSON.stringify(data) };
+    if (!hasError && passIsEqual && newPassword) {
+      const payload = { method: 'POST', body: JSON.stringify(newPassword) };
 
-      fetch('/admin/myprofile', payload)
+      fetch('/admin/newpassword', payload)
         .then(res => console.log(res))
         .catch(err => console.log(err));
-      console.log('SEND DATA', data);
+      console.log('SEND DATA', newPassword);
 
       // Reset Form field
       this.setState(initialState);
@@ -44,42 +43,42 @@ class UserForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <Input
-          label='Full Name'
-          placeholder='John Doe'
-          name='fullName'
-          type='text'
+          label='Current Password'
+          placeholder='Current Password'
+          name='currentPassword'
+          type='password'
           showError={this.state.showError}
           hasError={this.hasError}
           onChange={this.handleChange}
-          value={this.state.fullName}
+          value={this.state.currentPassword}
         />
 
         <Input
-          label='Email'
-          placeholder='john.doe@mail.com'
-          name='email'
-          type='email'
+          label='New Password'
+          placeholder='New Password'
+          name='newPassword'
+          type='password'
           showError={this.state.showError}
           hasError={this.hasError}
           onChange={this.handleChange}
-          value={this.state.email}
+          value={this.state.newPassword}
         />
 
         <Input
-          label='Phone'
-          placeholder='123 456-7890'
-          name='phone'
-          type='tel'
+          label='Repeat Password'
+          placeholder='Repeat Password'
+          name='repeatPassword'
+          type='password'
           showError={this.state.showError}
           hasError={this.hasError}
           onChange={this.handleChange}
-          value={this.state.phone}
+          value={this.state.repeatPassword}
         />
 
-        <button type='submit'>Create New User</button>
+        <button type='submit'>Confirm New Password</button>
       </form>
     );
   }
 }
 
-export default UserForm;
+export default PasswordForm;
--- client/src/components/ProfilePage/index.js
+++ client/src/components/ProfilePage/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import UserImage from './UserImage';
 import UserForm from './UserForm';
+import PasswordForm from './PasswordForm';
 
 const ProfilePage = () => (
   <>
     <h1>Profile Page</h1>
+    <UserImage />
     <UserForm />
+    <PasswordForm />
   </>
 );
 
