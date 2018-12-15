54fbb95ac0f2cd1939d1b561ea03ea3907d9779a
David De Wulf <contact.dewulf@gmail.com>
2018-12-14 17:28:46 -0800

    Add dummyDB

--- client/src/components/NewUpdate.js
+++ client/src/components/NewUpdate.js
@@ -1,56 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { parksDB } from '../dummyDB';
 
 export class newUpdate extends Component {
-  constructor(props){ 
-    super(props); 
-    this.handleFilter = this.handleFilter.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleFilter = this.handleFilter.bind(this);
     this.state = {
-        lists: [{text: "Municipal Rose Garden", key: "ROSE"}],
-        filteredLists: [],
-        selectedLists: [],
-        filter: '',
-    }
-}
+      lists: [],
+      filteredLists: [],
+      selectedLists: [],
+      filter: ''
+    };
+  }
 
-handleInput = e => { //generic handleInput handles the change for any input field using the name and the value properties.
-  const {name, value} = e.target;
-  console.log('name',name)
-  console.log('value',value)
-  this.setState({
+  componentDidMount() {
+    this.setState({ lists: parksDB });
+  }
+
+  handleInput = e => {
+    //generic handleInput handles the change for any input field using the name and the value properties.
+    const { name, value } = e.target;
+    this.setState({
       [name]: value
-  });
-}
+    });
+  };
 
-handleFilter = e => {
-  e.preventDefault();  
-  const filtered = this.state.lists.filter(item=> {
-      return item.text.includes(this.state.filter)
-  });
-  this.setState({
-    filteredLists: filtered,
-    filter: '',
-  });
-}
+  handleFilter = e => {
+    e.preventDefault();
+    const filtered = this.state.lists.filter(item => {
+      return item.name.toLowerCase().includes(this.state.filter.toLowerCase());
+    });
+    this.setState({
+      filteredLists: filtered,
+      filter: ''
+    });
+  };
   render() {
     return (
       <div>
         <div>
           <form onSubmit={this.handleFilter}>
-            <input 
-                name='filter'
-                value={this.state.filter} 
-                onChange={this.handleInput} 
-                type="text" id="filter" placeholder="Search Parks.."/>
-            <button type="submit">Search</button>
+            <input
+              name='filter'
+              value={this.state.filter}
+              onChange={this.handleInput}
+              type='text'
+              id='filter'
+              placeholder='Search Parks..'
+            />
+            <button type='submit'>Search</button>
           </form>
           <button>Select All</button>
           <ul>
-            {this.state.filteredLists.map((list, i) => <li key={i}>{list.text}</li>)}
+            {this.state.filteredLists.map(list => (
+              <li key={list._id}>
+                <span>{list.name}</span>-<span>{list.parkID}</span>
+              </li>
+            ))}
           </ul>
         </div>
-
       </div>
-    )
+    );
   }
 }
 
---
+++ client/src/dummyDB.js
@@ -0,0 +1,52 @@
+export const parksDB = [
+  {
+    _id: '5c1452b40571e5c107c1b43c',
+    parkID: 'IDE',
+    name: 'SPEEDBOLT'
+  },
+  {
+    _id: '5c1452b441bbfe1c2ac4ada4',
+    parkID: 'EBI',
+    name: 'UTARIAN'
+  },
+  {
+    _id: '5c1452b44620b715a1f75f7d',
+    parkID: 'UNQ',
+    name: 'TURNABOUT'
+  },
+  {
+    _id: '5c1452b415b77a4fe3af80df',
+    parkID: 'APP',
+    name: 'POOCHIES'
+  },
+  {
+    _id: '5c1452b4abd9c30e6dd896ae',
+    parkID: 'KIN',
+    name: 'ACUMENTOR'
+  },
+  {
+    _id: '5c1452b4fe02736ce53a1f7d',
+    parkID: 'ORB',
+    name: 'GEOFORMA'
+  },
+  {
+    _id: '5c1452b4ce5d22aa6d23eb8b',
+    parkID: 'NEW',
+    name: 'ONTAGENE'
+  },
+  {
+    _id: '5c1452b4b1665cac6f64c6e7',
+    parkID: 'PRO',
+    name: 'PASTURIA'
+  },
+  {
+    _id: '5c1452b41d942e81254f8eaf',
+    parkID: 'VAN',
+    name: 'QOT'
+  },
+  {
+    _id: '5c1452b4da93463398484c1f',
+    parkID: 'ZID',
+    name: 'ZILPHUR'
+  }
+];
