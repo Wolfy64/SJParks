9c2ae6f15a9a6eb2975841624d6acbe9f4ec0107
David De Wulf <contact.dewulf@gmail.com>
2018-12-21 15:15:57 -0800

    Add config folder

---
+++ client/src/config/messages.js
@@ -0,0 +1,8 @@
+export const msgErr = {
+  text: 'Minimum 3 characaters',
+  mail: 'Invalid email address',
+  phone: 'Invalid phone number',
+  pass: 'Minimum 6 characaters',
+  select: 'You must choose one option',
+  form: 'Something wrong, did you fill up everything correctly?'
+};
