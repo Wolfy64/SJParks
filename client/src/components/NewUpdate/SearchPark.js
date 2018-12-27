05041b942fa71f0e7b5a2904c35564fedbc7d73f
David De Wulf <contact.dewulf@gmail.com>
2018-12-27 14:05:22 -0800

    Add NewUpdate Components

--- client/src/components/NewUpdate/SearchPark.js
+++ client/src/components/NewUpdate/SearchPark.js
@@ -1,67 +1,47 @@
 import React, { Component } from 'react';
-import { parksDB } from '../../dummyDB';
+import ParkLi from './ParkLi';
+import Input from '../UI/Form/Input';
 
-export class newUpdate extends Component {
-  constructor(props) {
-    super(props);
-    this.handleFilter = this.handleFilter.bind(this);
-    this.state = {
-      lists: [],
-      filteredLists: [],
-      selectedLists: [],
-      filter: ''
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ lists: parksDB });
-  }
+class NewUpdate extends Component {
+  state = {
+    parksList: [],
+    filter: ''
+  };
 
   handleInput = e => {
-    //generic handleInput handles the change for any input field using the name and the value properties.
     const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
+    const { parks } = this.props;
+    const parksList = parks.filter(el =>
+      el.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    this.setState({ [name]: value, parksList });
   };
 
-  handleFilter = e => {
-    e.preventDefault();
-    const filtered = this.state.lists.filter(item => {
-      return item.name.toLowerCase().includes(this.state.filter.toLowerCase());
-    });
-    this.setState({
-      filteredLists: filtered,
-      filter: ''
-    });
-  };
   render() {
+    const { filter, parksList } = this.state;
+    const { addPark, addAllParks } = this.props;
+    const parkLi = parksList.map(el => (
+      <ParkLi key={el._id} park={el} clicked={() => addPark(el)} />
+    ));
+
     return (
       <div>
-        <div>
-          <form onSubmit={this.handleFilter}>
-            <input
-              name='filter'
-              value={this.state.filter}
-              onChange={this.handleInput}
-              type='text'
-              id='filter'
-              placeholder='Search Parks..'
-            />
-            <button type='submit'>Search</button>
-          </form>
-          <button>Select All</button>
-          <ul>
-            {this.state.filteredLists.map(list => (
-              <li key={list._id} onClick={() => this.props.addPark(list._id)}>
-                <span>{list.name}</span>-<span>{list.parkID}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Input
+          name='filter'
+          value={filter}
+          onChange={this.handleInput}
+          type='text'
+          placeholder='Search Parks by Name'
+          autoComplete='off'
+        />
+
+        <button onClick={addAllParks}>Select All</button>
+
+        <ul>{parkLi}</ul>
       </div>
     );
   }
 }
 
-export default newUpdate;
+export default NewUpdate;
