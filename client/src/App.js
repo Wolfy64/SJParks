82f3e00a050cba283dd123ab086cb39be2edfbd7
David De Wulf <contact.dewulf@gmail.com>
2019-01-02 17:35:22 -0800

    Change State

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 
 class App extends React.Component {
-  state = { isConnected: false };
+  state = { isConnected: true };
 
   render() {
     let routes = (
