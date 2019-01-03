7558ee6f8c30130b4087e96161110d00c2a07c85
Irina Mityugova <irishka2863@yahoo.com>
2019-01-03 12:13:37 -0800

    Debugging

--- controllers/router.js
+++ controllers/router.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
     })
   } else {
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build','index.html'));
+      res.sendFile(path.join(__dirname, 'client', 'public','index.html'));
     })
   }
   //------------------------------------------------------------------------
