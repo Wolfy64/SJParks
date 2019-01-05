b3e5817b47f717760cc70ddf6bc0d24794b93f5a
Henry O Zorrilla <hernyz37@gmail.com>
2019-01-05 14:57:07 -0800

    merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Parks from './components/ParksPage';
-import Users from './components/UsersPage';
-import Updates from './components/UpdatesPage';
-import NewUpdate from './components/NewUpdate';
-import ProfilePage from './components/ProfilePage';
-import PublicPage from './components/PublicPage';
-import NoMatch from './components/UI/NoMatch';
-import Login from './components/LoginPage';
-import { Route, Switch } from 'react-router-dom';
-
-import Layout from './components/Layout';
-
-class App extends React.Component {
-  state = { isConnected: false };
-
-  render() {
-    let routes = (
-      <Switch>
-        <Route path='/' component={PublicPage} exact />
-        <Route path='/login' component={Login} />
-        <Route component={NoMatch} />
-      </Switch>
-    );
-
-    if (this.state.isConnected) {
-      routes = (
-        <Layout>
-          <Switch>
-            <Route path='/admin/newupdate' component={NewUpdate} />
-            <Route path='/admin/updates' component={Updates} />
-            <Route path='/admin/parks' component={Parks} />
-            <Route path='/admin/users' component={Users} />
-            <Route path='/admin/profile' component={ProfilePage} />
-            <Route path='/' component={PublicPage} exact />
-            <Route component={NoMatch} />
-          </Switch>
-        </Layout>
-      );
-    }
-
-    return routes;
-  }
-}
-
-export default App;
-=======
 import React from 'react';
 import Parks from './components/ParksPage';
 import Users from './components/UsersPage';
@@ -92,4 +44,3 @@ class App extends React.Component {
 }
 
 export default App;
->>>>>>> 46ab335183b596282481ad22bca58f865dbe5a7a
--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-console.log('>> TEST: Running server.js');
-const http = require('http');
-const mongoose = require('mongoose');
-const config = require('./config');
-
-// Initialize database connection - throws if database connection can't be
-// established
-mongoose.connect(config.mongoUrl);
-mongoose.Promise = Promise;
-
-console.log('>> PASS: server.js connected to mongoose');
-
-// Create Express web app
-const app = require('./webapp');
-
-// Create an HTTP server and listen on the configured port
-const server = http.createServer(app);
-server.listen(config.port, function() {
-  console.log('>> Express server listening on port:' + config.port);
-});
-=======
 const http = require('http');
 const mongoose = require('mongoose');
 const config = require('./config');
@@ -46,4 +24,3 @@ const server = http.createServer(app);
 server.listen(config.port, () =>
   console.log(`Server listening on port: ${config.port}`)
 );
->>>>>>> 46ab335183b596282481ad22bca58f865dbe5a7a
