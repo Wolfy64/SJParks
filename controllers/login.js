922f43044dca48bba8b406a32abc9a65053e540b
Henry O Zorrilla <hernyz37@gmail.com>
2019-01-05 15:01:13 -0800

    merge conflicts

--- controllers/login.js
+++ controllers/login.js
@@ -2,13 +2,13 @@ const db = require("../models");
 
 //  Authorize the login information
 exports.validate = function (request, response) {
-    console.log('>> TEST validating...');
+    console.log('validating...');
     if (!request.body.username || !request.body.psw) {
         console.log('no username or password');
         response.redirect('/login');
     }
     else {
-        console.log(`username:${request.body.username}  password:${request.body.psw}`);
+        console.log(`usernaem:${request.body.username}  password:${request.body.psw}`);
         db.User.findOne({username: request.body.username}, function(err, user) {
             if (err) response.redirect('/login');
             if (user && user.validate_password(request.body.psw)){
