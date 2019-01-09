08166bf4e6686e96899c4b0f8e46ce6279e94783
Henry O Zorrilla <hernyz37@gmail.com>
2019-01-08 16:53:30 -0800

    cleaning before pulling

---
+++ config/index.js
@@ -0,0 +1,39 @@
+require('dotenv-safe').load();
+
+const cfg = {};
+
+// HTTP Port for sjparks.c0d3.com
+cfg.port = process.env.PORT || 96089;
+
+// A random string that will help generate secure one-time passwords and
+// HTTP sessions
+cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+//
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+
+// A Twilio number you control - choose one from:
+// https://www.twilio.com/user/account/phone-numbers/incoming
+// Specify in E.164 format, e.g. "+16519998877"
+cfg.twilioNumber = process.env.TWILIO_NUMBER;
+
+// MongoDB connection string - MONGO_URL is for local dev,
+// MONGOLAB_URI is for the MongoLab add-on for Heroku deployment
+cfg.mongoUrl =
+  process.env.MONGODB_URI ||
+  process.env.MONGO_URL ||
+  'mongodb://localhost:27017'; // default
+
+// MongoDB connection string for test purposes
+cfg.mongoUrlTest = 'mongodb://localhost/sj-parks';
+
+cfg.clientPath = process.env.NODE_ENV === 'production' ? 'build' : 'public';
+
+// Export configuration object
+module.exports = cfg;
