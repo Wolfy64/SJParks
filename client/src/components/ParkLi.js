2f168464cf7b0091c1f6c450d60cd1f4b638c1f6
Irina Mityugova <irishka2863@yahoo.com>
2019-01-09 18:11:26 -0800

    Added Styles

--- client/src/components/ParkLi.js
+++ client/src/components/ParkLi.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Label = styled.div`
+  background-color: ${props => props.theme.colors.success};
+  color: ${props => props.theme.colors.light};
+  padding: 7px;
+  margin: 5px 0;
+  border-radius: 5px;
+`
 const ParkLi = props => (
-  <li onClick={props.clicked}>
-    <span>{props.park.name}</span>-<span>{props.park.parkID}</span>
-  </li>
+  <Label>
+    <li onClick={props.clicked}>
+      <span>{props.park.name}</span>
+    </li>
+  </Label>
 );
 
 export default ParkLi;
--- client/src/components/ParksPage/index.js
+++ client/src/components/ParksPage/index.js
@@ -6,10 +6,25 @@ import SearchPark from '../SearchPark';
 import SelectedPark from '../SelectedPark';
 import { parksDB } from '../../dummyDB';
 import Button from '../UI/Generic/Button';
+import styled from 'styled-components';
+import { assertDeclareModule } from 'babel-types';
+
+const Col1 = styled.div`
+  min-width: 350px;
+  padding: 30px;
+  float: left;
+  margin: 0 20px 0 0;
+`
+const Col2 = styled.div`
+  min-width: 350px;
+  padding: 30px;
+  height: 100%;
+  float: left;
+  background-color: ${props => props.theme.colors.lightbg};
+`
 
 const initialState = {
   parks: [],
-  parkSelected: [],
   showErrors: false,
   newPark: '',
   parkId: ''
@@ -22,27 +37,10 @@ export default class Parks extends Component {
     this.setState({ parks: parksDB });
   }
 
-  handleAddPark = park => {
-    const { parkSelected } = this.state;
-    const isSelected = parkSelected.find(el => el._id === park._id);
-
-    if (!isSelected) this.setState({ parkSelected: [...parkSelected, park] });
-  };
-
-  handleAddAllPark = () => {
-    this.setState({ parkSelected: [...this.state.parks] });
-  };
-
   handleDeletePark = park => {
-    this.setState({
-      parkSelected: [
-        ...this.state.parkSelected.filter(el => el._id !== park._id)
-      ]
-    });
-  };
-
-  handleDeleteAddAllPark = () => {
-    this.setState({ parkSelected: [] });
+    if (window.confirm("Do you really want to delete ".concat(park.name).concat(" from the system?"))) { 
+      console.log('>> ', park.name, ' was removed.')
+    }
   };
 
   handleChange = e => {
@@ -101,40 +99,34 @@ export default class Parks extends Component {
   render() {
     return (
       <div>
-        <h2>List</h2>
-        <form onSubmit={this.handleSubmit}>
-          <Input
-            name='newPark'
-            value={this.state.newPark}
-            onChange={this.handleChange}
-            type='text'
-            placeholder='New Park...'
-            autoComplete='off'
+        <Col1>
+          <form onSubmit={this.handleSubmit}>
+            <Input
+              name='newPark'
+              value={this.state.newPark}
+              onChange={this.handleChange}
+              type='text'
+              placeholder='New Park...'
+              autoComplete='off'
+            />
+            <Input
+              name='parkId'
+              value={this.state.parkId}
+              onChange={this.handleChange}
+              type='text'
+              placeholder='Park Id...'
+              autoComplete='off'
+            />
+
+            <Button name='Create a new park' type='submit' />
+          </form>
+        </Col1>
+        <Col2>
+          <SearchPark
+            parks={this.state.parks}
+            addPark={park => this.handleDeletePark(park)}
           />
-          <Input
-            name='parkId'
-            value={this.state.parkId}
-            onChange={this.handleChange}
-            type='text'
-            placeholder='Park Id...'
-            autoComplete='off'
-          />
-
-          <Button name='Create a new park' type='submit' />
-        </form>
-
-        <h2>Filter</h2>
-        <SearchPark
-          parks={this.state.parks}
-          addPark={park => this.handleAddPark(park)}
-          addAllParks={this.handleAddAllPark}
-        />
-
-        <SelectedPark
-          parks={this.state.parkSelected}
-          deletePark={park => this.handleDeletePark(park)}
-          deleteAllParks={this.handleDeleteAddAllPark}
-        />
+        </Col2>
       </div>
     );
   }
