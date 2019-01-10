a4e1419ec64218b5d15d6649098b3b0d01bfab03
David De Wulf <contact.dewulf@gmail.com>
2019-01-10 14:52:03 -0800

    Refactor styled

--- client/src/components/ParkLi.js
+++ client/src/components/ParkLi.js
@@ -2,32 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Label = styled.div`
-  background-color: ${(props) => {
-    console.log(props)
-    if(props.selected) {
-      return props.theme.colors.success
-    } else {
-      return props.theme.colors.lightbg
-    }
-  }};
-  color: ${(props) => {
-    console.log(props)
-    if(props.selected) {
-      return props.theme.colors.light
-    } else {
-      return props.theme.colors.dark
-    }
-  }};
+  background-color: ${props =>
+    props.selected ? props.theme.colors.success : props.theme.colors.lightbg};
+  color: ${props =>
+    props.selected ? props.theme.colors.light : props.theme.colors.dark};
   padding: 7px;
   margin: 5px 0;
   border-radius: 5px;
   display: flex;
   justify-content: space-between;
-`
+`;
+
 const ParkLi = props => (
   <Label selected={props.selected} onClick={props.clicked}>
-      <span>{props.park.name} - {props.park.parkID}</span>
-      {props.selected? <i class="fa fa-times"></i> : <i class="fa fa-plus"></i>}
+    <span>
+      {props.park.name} - {props.park.parkID}
+    </span>
+    {props.selected ? <i class='fa fa-times' /> : <i class='fa fa-plus' />}
   </Label>
 );
 
