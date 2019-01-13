4ec942d97d096a1c29a132c467d115162df8ea68
Henry O Zorrilla <thmthmnky@gmail.com>
2019-01-12 20:19:10 -0800

    merging backend and front end

--- Routes.js
+++ Routes.js
@@ -10,7 +10,7 @@ const {
 console.log(`>[ROUTES:010:030]> Configuring Routes...`);
 
 // @route /register
-router.get('/register', (req, res) => res.render('register')); // api.
+router.get('/register', (req, res) => res.render('register')); // api.users.register
 
 // @route /login
 router.post('/login', api.users.login);
@@ -22,84 +22,84 @@ router.get('/welcome', (req, res) => res.render('welcome'));
 router.post('/logout', api.users.logout)
 
 // @route /dashboard
-router.get('/admin/updates', ensureAuthenticated, (req, res) => res.render('dashboard', {
+router.get('/admin/:userID/updates', ensureAuthenticated, (req, res) => res.render('adminDashboard', {
   user: req.user
 }));
 
 // @route /api/*
-/**/
+/*Need to be White Listed: "/api/parks", */
 // router.all('/api/*', ensureAuthenticated);
 
 // @route /api
 router.get('/api', api.index.index);
 
 // @route /api/user
-router.route('/api/user')
+router.route('/api/users')
   .get(api.users.index)
   .post(api.users.register);
 
   // @route /api/user/_id
-router.route('/api/user/:userId')
+router.route('/api/users/:userId')
   .get(api.users.read)
   .put(api.users.update)
   .delete(api.users.destroy);
 
-//  // @route /api/user/_id/park
-// router.route('/api/user/:userId/park')
+//  // @route /api/users/_id/park
+// router.route('/api/users/:userId/park')
 //   .get(api.users.readAllParks);
 
-// // @route /api/user/_id/message
-// router.route('/api/user/:userId/message')
+// // @route /api/users/_id/message
+// router.route('/api/users/:userId/message')
 //   .get(api.users.readAllMessages);
 
-// // @route /api/user/_id/park/_id
-// router.route('/api/user/:userId/park/:parkId')
+// // @route /api/users/_id/park/_id
+// router.route('/api/users/:userId/park/:parkId')
 //   .get(api.users.findPark);
 
 // // @route /api/user/_id/message/_id
-// router.route('/api/user/:userId/message/:messageId')
+// router.route('/api/users/:userId/message/:messageId')
 //   .get(api.users.findMessage);
 
 // @route /api/park
-router.route('/api/park')
+router.route('/api/parks')
   .get(api.parks.index)
   .post(api.parks.create);
 
-  // @route /api/park/_id/
-router.route('/api/park/:id')
+  // @route /api/parks/_id/
+router.route('/api/parks/:id')
   // .get(api.parks.read)
   // .put(api.parks.update)
   // .delete(api.parks.destroy);
 
-// @route /api/message
-router.route('/api/message')
+// @route /api/messages
+router.route('/api/messages')
   // .get(api.messages.index)
   // .post(api.messages.create);
 
-// @route /api/message/_id
-router.route('/api/message/:id')
+// @route /api/messages/_id
+router.route('/api/messages/:messagId')
   // .get(api.messages.read)
   // .put(api.messages.update)
   // .delete(api.messages.destroy);
 
-// // @route /api/messageLog
-// router.route('/api/messageLog')
+// // @route /api/messageLogs
+// router.route('/api/messageLogs')
 //   .get(api.messageLogs.index)
 //   .post(api.messageLogs.create);
 
 // // @route /api/messageLog/_id
-// router.route('/api/messageLog/:id')
+// router.route('/api/messageLogs/:messageLogId')
 //   .get(api.messageLogs.read)
 //   .put(api.messageLogs.update)
 //   .delete(api.messageLogs.destroy);
 
-// // @route /api/subscriptionLog
-// router.route('/api/subscriptionLog')
+// // @route /api/subscriptionLogs
+// router.route('/api/subscriptionLogs')
 //   .get(api.subscriptionLogs.index)
 //   .post(api.subscriptionLogs.create);
 
-// // @route /api/subscriptionLog/_id
-// router.route('/api/subscriptionLog/:id')
+// // @route /api/subscriptionLogs/_id
+// router.route('/api/subscriptionLogs/:subscriptionLogId')
 //   .get(api.subscriptionLogs.read)
 //   .put(api.subscriptionLogs.update)
 //   .delete(api.subscriptionLogs.destroy);
