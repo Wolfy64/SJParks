6089e79d990fd717359c2df19d5d4e09e6de6091
David De Wulf <contact.dewulf@gmail.com>
2019-01-13 16:14:31 -0800

    Add Max-Screen break

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
@@ -7,13 +8,22 @@ import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
 // import * as serviceWorker from './serviceWorker';
 
+const Container = styled.div`
+  max-width: ${props => props.theme.displays.hd};
+  margin: auto;
+
+  @media screen and (min-width: ${props => props.theme.displays.hd}) {
+    border: 1px solid ${props => props.theme.colors.dark};
+  }
+`;
+
 const app = (
   <BrowserRouter>
     <ThemeProvider theme={theme}>
-      <>
+      <Container>
         <GlobalStyle />
         <App />
-      </>
+      </Container>
     </ThemeProvider>
   </BrowserRouter>
 );
