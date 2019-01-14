c31309651f5ce4a179b5cb6f5deb1c7148d659a2
David De Wulf <contact.dewulf@gmail.com>
2019-01-14 09:44:42 -0800

    Add Style

--- client/src/components/PublicPage/About.js
+++ client/src/components/PublicPage/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import phone from '../../images/phone.png';
+import phone from '../../img/phone.png';
+import sms from '../../img/sms.jpg';
 
 const PresentationBox = styled.main`
   display: grid;
@@ -8,9 +9,28 @@ const PresentationBox = styled.main`
   justify-items: center;
   height: 70vh;
 
-  img {
+  .imgPhone {
     width: 300px;
     margin-top: -11rem;
+  }
+
+  .imgSms {
+    display: none;
+  }
+
+  @media screen and (max-width: ${props => props.theme.displays.tablet}) {
+    grid-template-columns: 1fr;
+    height: auto;
+
+    .imgPhone {
+      display: none;
+    }
+
+    .imgSms {
+      display: block;
+      width: 300px;
+      margin: auto;
+    }
   }
 `;
 
@@ -27,32 +47,44 @@ const Summary = styled.div`
     line-height: 1.5;
     padding-left: 1rem;
   }
+
+  @media screen and (max-width: ${props => props.theme.displays.tablet}) {
+    padding: 1rem;
+
+    ul {
+      padding-left: initial;
+    }
+  }
 `;
 
-const About = () => (
-  <PresentationBox id='about'>
-    <Summary>
-      <p>
-        Have your visited the park onlyto find out hat is closed for
-        maintenance, or even worse, openand unsafe? We provide a solution for
-        you to stay informed.
-      </p>
-      <p>
-        All it takes is a keyword of the park, you're in to receive instant
-        update.
-      </p>
-      <ul>
-        <li>
-          1. Urgent update come to your phone directly form San Jose Park
-          Administration
-        </li>
-        <li>2. Know about events at the park you plan to visit.</li>
-        <li>3. Get involves in San Jose community.</li>
-      </ul>
-    </Summary>
+const About = () => {
+  return (
+    <PresentationBox id='about'>
+      <img className='imgSms' src={sms} alt='Sms Eg' />
 
-    <img src={phone} alt='Phone Eg' />
-  </PresentationBox>
-);
+      <Summary>
+        <p>
+          Have your visited the park onlyto find out hat is closed for
+          maintenance, or even worse, openand unsafe? We provide a solution for
+          you to stay informed.
+        </p>
+        <p>
+          All it takes is a keyword of the park, you're in to receive instant
+          update.
+        </p>
+        <ul>
+          <li>
+            1. Urgent update come to your phone directly form San Jose Park
+            Administration
+          </li>
+          <li>2. Know about events at the park you plan to visit.</li>
+          <li>3. Get involves in San Jose community.</li>
+        </ul>
+      </Summary>
+
+      <img className='imgPhone' src={phone} alt='Phone Eg' />
+    </PresentationBox>
+  );
+};
 
 export default About;
