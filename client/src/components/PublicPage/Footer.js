2f6dcbf44bb04c66fb6cb1b711638dbb0111eb8a
David De Wulf <contact.dewulf@gmail.com>
2019-01-14 09:47:12 -0800

    Add Style

--- client/src/components/PublicPage/Footer.js
+++ client/src/components/PublicPage/Footer.js
@@ -2,12 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.footer`
+  font-size: 0.8rem;
   color: ${props => props.theme.colors.light};
   background-color: ${props => props.theme.colors.primary};
   height: 10vh;
   display: flex;
   align-items: center;
   padding-left: 5rem;
+
+  @media screen and (max-width: ${props => props.theme.displays.tablet}) {
+    padding: 1rem;
+    height: auto;
+  }
 `;
 
 const Footer = () => (
