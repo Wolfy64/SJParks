e0e723a1692104da4a54a2c96a9aca8c1a287ebc
David De Wulf <contact.dewulf@gmail.com>
2019-01-14 10:04:20 -0800

    Refactor Css & add alt img

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import './topnav.css';
+import styled from 'styled-components';
+// import './topnav.css';
 import { Link } from 'react-router-dom';
 
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  height: 50px;
+  padding: 10px;
+
+  img {
+    height: 36px;
+    width: auto;
+    border-radius: 50%;
+  }
+`;
+
 const profileImg = require('../../img/Placeholder2.jpg');
- 
+
 const TopNav = () => (
-  <div className='top-nav'>
-    <nav className='navbar'>
-      <li>
-        <Link to='/admin/profile'><img src={profileImg}></img></Link>
-      </li>
-    </nav>
-  </div>
+  <Nav className='navbar'>
+    <Link to='/admin/profile'>
+      <img src={profileImg} alt='User Avatar' />
+    </Link>
+  </Nav>
 );
 
 export default TopNav;
