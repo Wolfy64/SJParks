9f1c33824db091730ad31bf52a0cebec5b0ead09
David De Wulf <contact.dewulf@gmail.com>
2019-01-16 15:08:33 -0800

    Add user login

--- config/passport.js
+++ config/passport.js
@@ -4,34 +4,27 @@ const User = require('../models/User');
 
 module.exports = passport => {
   passport.use(
-    new LocalStrategy({
-      usernameField: 'userName'
-    }, (userName, password, done) => {
+    new LocalStrategy(
+      { usernameField: 'email' },
+      async (email, password, done) => {
+        // Match user
+        const user = await User.findOne({ email });
 
-      // Match user
-      User.findOne({
-        userName: userName
-      }).then(user => {
-        if (!user) {
+        if (!user)
           return done(null, false, {
             message: 'That email is not registered'
           });
-        }
 
         // Match password
-        bcrypt.compare(password, user.password, (err, isMatch) => {
+        bcrypt.compare(password, user.password, async (err, isMatch) => {
           if (err) throw err;
-          if (isMatch) {
-            console.log(`>[PASS:048:033]> Welcome User! --> @id:${user._id} `)
-            return done(null, user);
-          } else {
-            return done(null, false, {
-              message: 'Password incorrect'
-            });
-          }
+
+          isMatch
+            ? done(null, user)
+            : done(null, false, { message: 'Password incorrect' });
         });
-      });
-    })
+      }
+    )
   );
 
   passport.serializeUser((user, done) => {
