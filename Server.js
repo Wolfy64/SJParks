dac4dbb6e8298a65a42248d0b6edeb4a9d56d725
Henry O Zorrilla <hernyz37@gmail.com>
2019-01-16 17:08:07 -0800

    refactoring to add userAUTH: wip

--- Server.js
+++ Server.js
@@ -1,30 +1,29 @@
 require('dotenv-safe').load();
 const config = require('./config');
-// process.env.NODE_ENV === "development";
 console.log(`>[SERVER:004:025]> Running index in ${process.env.NODE_ENV} mode...`);
+
 //----------------------------------------------------------------------------------------------------------------------------------------------
 //************************************************************** Connect MongoDB ***************************************************************
 //----------------------------------------------------------------------------------------------------------------------------------------------
+const mongoose = require('mongoose');
+const db = config.keys.mongoUrl;
+
 const opts = {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
 }
 
-const mongoose = require('mongoose');
-const db = config.keys.mongoUrl;
+mongoose.Promise = global.Promise;
 
 mongoose.connect(db, opts)
   .then(() => console.log(`>[SERVER:020:020]> MongoDB Connected @uri: ${db}...`))
   .catch(err => console.error(`>[SERVER:021:044]> An error occured while attempting to connect to MongoDB with @uri: ${db}. Error thrown: ${err.message}...`));
 
-mongoose.Promise = global.Promise;
-
 //----------------------------------------------------------------------------------------------------------------------------------------------
-//*********************************************************** Deploy Express Webapp ************************************************************
+//*********************************************************** Deploy Express WebApp ************************************************************
 //----------------------------------------------------------------------------------------------------------------------------------------------
 const http = require('http');
 const webapp = require('./App');
-const server = http.createServer(webapp);
-const port = require('./config').keys.port;
-server.listen(port, () => console.log(`>[SERVER:032:054]> Express Server Deploy
+const server = http.createServer(webapp);;
+server.listen(config.keys.port, () => console.log(`>[SERVER:032:054]> Express Server Deployed @url: http://localhost:${config.keys.port}...`));
