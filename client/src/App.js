6f9613f3f68475a6b8943be1a8246990fb8a2fe6
David De Wulf <contact.dewulf@gmail.com>
2019-01-29 10:52:56 -0800

    Delete token if TS is expired

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import jwt_decode from 'jwt-decode';
 import Parks from './components/ParksPage';
 import Users from './components/UsersPage';
 import Updates from './components/UpdatesPage';
@@ -12,27 +13,39 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 
 class App extends React.Component {
-  state = { isConnected: true };
+  state = { isAdmin: false };
+
+  componentDidMount() {
+    let token = localStorage.getItem('token');
+    if (token) {
+      token = jwt_decode(token);
+      // Chek if the token is expired
+      const isValid = Date.now() / 1000 < token.exp;
+      if (!isValid) localStorage.removeItem('token');
+      this.setState({ isAdmin: isValid });
+    }
+  }
 
   render() {
+    const { isAdmin } = this.state;
+
     let routes = (
       <Switch>
-        <Route path='/' component={PublicPage} exact />
-        <Route path='/login' component={Login} />
+        <Route path="/" component={PublicPage} exact />
+        <Route path="/login" component={Login} />
         <Route component={NoMatch} />
       </Switch>
     );
 
-    if (this.state.isConnected) {
+    if (isAdmin) {
       routes = (
         <Layout>
           <Switch>
-            <Route path='/admin/newupdate' component={NewUpdate} />
-            <Route path='/admin/updates' component={Updates} />
-            <Route path='/admin/parks' component={Parks} />
-            <Route path='/admin/users' component={Users} />
-            <Route path='/admin/profile' component={ProfilePage} />
-            <Route path='/' component={PublicPage} exact />
+            <Route path="/admin/:id/newupdate" component={NewUpdate} />
+            <Route path="/admin/:id/updates" component={Updates} />
+            <Route path="/admin/:id/parks" component={Parks} />
+            <Route path="/admin/:id/users" component={Users} />
+            <Route path="/admin/:id/profile" component={ProfilePage} />
             <Route component={NoMatch} />
           </Switch>
         </Layout>
