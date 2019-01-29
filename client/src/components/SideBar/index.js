15eb07482c7ba1e8528f8da406b9dd78a0ec7deb
David De Wulf <contact.dewulf@gmail.com>
2019-01-28 18:40:52 -0800

    Add userID into URL

--- client/src/components/SideBar/index.js
+++ client/src/components/SideBar/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import jwt_decode from 'jwt-decode';
 import styled from 'styled-components';
 import NavButton from '../UI/Generic/NavButton';
 
 const SideNav = styled.div`
-  border-right: solid 3px ${props=>props.theme.colors.primary};
+  border-right: solid 3px ${props => props.theme.colors.primary};
   position: fixed;
   top: 0;
-  background: ${props=>props.theme.colors.dark};
+  background: ${props => props.theme.colors.dark};
   width: 150px;
   height: 100vh;
-  color: ${props=>props.theme.colors.lightbg};
+  color: ${props => props.theme.colors.lightbg};
 
   .logout {
     position: absolute;
     bottom: 10px;
     width: inherit;
-  };
+  }
 
   .title {
     text-align: center;
@@ -23,35 +24,51 @@ const SideNav = styled.div`
     h1 {
       font-size: 1.8em;
       margin-bottom: 0.3rem;
-    };
-  };
+    }
   }
 `;
 
-const SideBar = () => (
-  <SideNav>
-    <div className='title'>
-      <h1>SJParks</h1>
-      <p>Admin</p>
-    </div>
-    <ul className='navbar-nav'>
-      <li>
-        <NavButton to='/admin/updates' name='Updates' action='updatePage' />
-      </li>
-      <li>
-        <NavButton to='/admin/parks' name='Parks' action='parkPage' />
-      </li>
-      <li>
-        <NavButton to='/admin/users' name='Users' action='userPage' />
-      </li>
-    </ul>
+const SideBar = () => {
+  const token = localStorage.getItem('token');
+  const userID = jwt_decode(token).user._id;
 
-    <div className='logout'>
-      <form action="/login/out" method="POST">
-        <NavButton type='submit' name='Logout' action='logoutPage' />
-      </form>
-    </div>
-  </SideNav>
-);
+  return (
+    <SideNav>
+      <div className="title">
+        <h1>SJParks</h1>
+        <p>Admin</p>
+      </div>
+      <ul className="navbar-nav">
+        <li>
+          <NavButton
+            to={`/admin/${userID}/updates`}
+            name="Updates"
+            action="updatePage"
+          />
+        </li>
+        <li>
+          <NavButton
+            to={`/admin/${userID}/parks`}
+            name="Parks"
+            action="parkPage"
+          />
+        </li>
+        <li>
+          <NavButton
+            to={`/admin/${userID}/users`}
+            name="Users"
+            action="userPage"
+          />
+        </li>
+      </ul>
+
+      <div className="logout">
+        <form action="/login/out" method="POST">
+          <NavButton type="submit" name="Logout" action="logoutPage" />
+        </form>
+      </div>
+    </SideNav>
+  );
+};
 
 export default SideBar;
