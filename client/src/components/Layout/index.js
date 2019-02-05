48de0c7d88eaea19aeebf05159c2bba8de191595
Irina Mityugova <irishka2863@yahoo.com>
2019-02-05 15:12:46 -0800

    mobile page

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -5,6 +5,9 @@ import SideBar from '../SideBar';
 
 const Page = styled.div`
   margin: 0 80px 0 240px;
+  @media screen and (max-width: ${(props) => props.theme.displays.mobileL}) {
+    margin: 50px 10px;
+  }
 `;
 
 const Layout = ({ children }) => (
