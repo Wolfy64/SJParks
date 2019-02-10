e3bae520c816c3b45356786e8a449a68c979843c
Irina Mityugova <irishka2863@yahoo.com>
2019-02-10 11:58:15 -0800

    merge conflicts

--- client/src/components/UsersPage/index.js
+++ client/src/components/UsersPage/index.js
@@ -4,12 +4,6 @@ import UsersList from './UsersList';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-<<<<<<< HEAD
-  float: left;
-  width: 470px;
-  .usersForm {
-    width: 300px;
-=======
   width: 50%;
   .usersForm{
     max-width: 300px;
@@ -24,19 +18,9 @@ const Wrapper = styled.div`
     .usersList{
       margin: 40px 0;
     }
->>>>>>> 7c0f245f40c570c4fcf22fdd8356d20f1b53769c
-  }
-
-<<<<<<< HEAD
-  @media screen and (max-width: ${props => props.theme.displays.mobileL}) {
-    .usersForm {
-      margin: auto;
-    }
   }
 `;
 
-=======
->>>>>>> 7c0f245f40c570c4fcf22fdd8356d20f1b53769c
 const Users = () => (
   <>
     <Wrapper>
