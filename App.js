21541f1e46066eacb56b2690a3c2429ecc586fb4
Henry Zorrilla <thmthmnky@gmail.com>
2019-02-09 20:25:27 -0800

    Removed ghostCode from Server, App, Passport

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 require('dotenv-safe').load();
 const path = require('path');
 const cors = require('cors');
+const flash = require('connect-flash');
 const logger = require('morgan');
 const express = require('express');
 const addRequestId = require('express-request-id')();
@@ -9,34 +10,60 @@ const addRequestId = require('express-request-id')();
 /** Load Configurations */
 const router = require('./Routes');
 const config = require('./config');
+
 let app = express();
 
+/** View Engine */
 app.use(cors());
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, config.keys.path)));
 
 /** Logger */
 app.use(addRequestId);
-logger.token('id', (req) => req.sessionID.split('-')[0]);
-app.use(logger('combined'/*, {skip: (req, res) => res.statusCode < 400 }*/));
+logger.token('id', req => req.sessionID.split('-')[0]);
+app.use(logger('combined' /*, {skip: (req, res) => res.statusCode < 400 }*/));
 app.use(logger('> [:date[iso]] :method :url :status :response-time ms - :res[content-length] >'));
 
-/** Parser */
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
-app.use(formData.parse());
-app.use(express.static(path.join(__dirname, config.keys.path)));
+/** Flash */
+app.use(flash());
+app.use(function(req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
-/** View Engine */
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, config.keys.path)));
+/** Parser */
+app.use(formData.parse());
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+);
 
 /** Passport */
 config.passport(app);
 
 /** Routes */
-app.use('/auth', router.auth);
+app.use(function(req, res, next) {
+	console.log('request', req.url, req.body, req.method);
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-token');
+	if (req.method === 'OPTIONS') {
+		res.end();
+	} else {
+		next();
+	}
+});
+
+// // // @desc Configuring Session Handling
+// // app.use((req, res, next) =>{
+// //   if (req.session.admin) next();
+// //   else res.redirect('api/user/login');
+// // });
+
+app.use('/admin', router.auth);
 app.use('/api', router.api);
 
 /** Error Handlers */
--- config/passport.js
+++ config/passport.js
@@ -32,16 +32,6 @@ module.exports = (app) => {
 
 	app.use(session(sessOpts));
 
-	/** Flash */
-
-	app.use(flash());
-	app.use(function(req, res, next) {
-		res.locals.success_msg = req.flash('success_msg');
-		res.locals.error_msg = req.flash('error_msg');
-		res.locals.error = req.flash('error');
-		next();
-	});
-
 	/** Serialize user to user._id  */
 	passport.serializeUser((user, done) => {
 		return done(null, user._id);
