c4233fc4f26354cb5d4f904e5870e89b0cc0b575
David De Wulf <contact.dewulf@gmail.com>
2019-02-11 22:46:56 -0800

    Refactor

--- client/src/components/ProfilePage/PasswordForm.js
+++ client/src/components/ProfilePage/PasswordForm.js
@@ -2,112 +2,107 @@ import React from 'react';
 import Input from '../UI/Form/Input';
 import errorFormHandler from '../../utils/errorFormHandler';
 import isFormValid from '../../utils/isFormValid';
-import Button from '../UI/Generic/Button'
+import Button from '../UI/Generic/Button';
 import styled from 'styled-components';
-
+import makeRequest from '../../utils/makeRequest';
 
 const Wrapper = styled.div`
- width: 280px;
- margin: 10px;
+  width: 280px;
+  margin: 10px;
 `;
 
 const initialState = {
- currentPassword: '',
- newPassword: '',
- repeatPassword: '',
- showErrors: false,
- formErrors: false
+  currentPassword: '',
+  newPassword: '',
+  repeatPassword: '',
+  showErrors: false,
+  formErrors: false
 };
 
 class PasswordForm extends React.Component {
- state = initialState;
+  state = initialState;
 
- handleChange = e => {
-   const { name, type, value } = e.target;
+  handleChange = e => {
+    const { name, type, value } = e.target;
 
-   this.setState({
-     [name]: value,
-     formErrors: {
-       ...this.state.formErrors,
-       [name]: errorFormHandler(type, value)
-     }
-   });
- };
+    this.setState({
+      [name]: value,
+      formErrors: {
+        ...this.state.formErrors,
+        [name]: errorFormHandler(type, value)
+      }
+    });
+  };
 
- handleSubmit = e => {
-   e.preventDefault();
-   const { formErrors, newPassword, repeatPassword } = this.state;
-   const passIsEqual = repeatPassword === newPassword;
-   const isValid = isFormValid(formErrors, newPassword) && passIsEqual;
+  handleSubmit = e => {
+    e.preventDefault();
+    const { formErrors, newPassword, repeatPassword } = this.state;
+    const passIsEqual = repeatPassword === newPassword;
+    const isValid = isFormValid(formErrors, newPassword) && passIsEqual;
 
-   if (!passIsEqual) {
-     this.setState({
-       formErrors: {
-         ...this.state.formErrors,
-         repeatPassword: 'Password must be identical'
-       }
-     });
-   }
+    if (!passIsEqual) {
+      this.setState({
+        formErrors: {
+          ...this.state.formErrors,
+          repeatPassword: 'Password must be identical'
+        }
+      });
+    }
 
-   isValid
-     ? this.handleSendForm(newPassword)
-     : this.setState({ showErrors: true });
- };
+    if (isValid) {
+      const userId = 12345;
+      makeRequest(`/admin/users/${userId}/contact`, 'PUT', newPassword);
+      console.log('SEND DATA', newPassword);
 
- handleSendForm = dataForm => {
-   const payload = { method: 'POST', body: JSON.stringify(dataForm) };
+      // Reset Form field
+      this.setState(initialState);
+    } else {
+      this.setState({ showErrors: true });
+    }
+  };
 
-   fetch('/admin/newpassword', payload)
-     .then(res => console.log(res))
-     .catch(err => console.log(err));
-   console.log('SEND DATA', dataForm);
+  render() {
+    const { formErrors, showErrors } = this.state;
+    const hasErrors = showErrors && formErrors;
 
-   // Reset Form field
-   this.setState(initialState);
- };
+    return (
+      <Wrapper>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            label="Current Password"
+            placeholder="Current Password"
+            name="currentPassword"
+            type="password"
+            onChange={this.handleChange}
+            value={this.state.currentPassword}
+            error={hasErrors ? formErrors.currentPassword : null}
+          />
 
- render() {
-   const { formErrors, showErrors } = this.state;
-   const hasErrors = showErrors && formErrors;
+          <Input
+            label="New Password"
+            placeholder="New Password"
+            name="newPassword"
+            type="password"
+            onChange={this.handleChange}
+            value={this.state.newPassword}
+            error={hasErrors ? formErrors.newPassword : null}
+          />
 
-   return (
-     <Wrapper>
-     <form onSubmit={this.handleSubmit}>
-       <Input
-         label='Current Password'
-         placeholder='Current Password'
-         name='currentPassword'
-         type='password'
-         onChange={this.handleChange}
-         value={this.state.currentPassword}
-         error={hasErrors ? formErrors.currentPassword : null}
-       />
+          <Input
+            label="Repeat Password"
+            placeholder="Repeat Password"
+            name="repeatPassword"
+            type="password"
+            onChange={this.handleChange}
+            value={this.state.repeatPassword}
+            error={hasErrors ? formErrors.repeatPassword : null}
+          />
 
-       <Input
-         label='New Password'
-         placeholder='New Password'
-         name='newPassword'
-         type='password'
-         onChange={this.handleChange}
-         value={this.state.newPassword}
-         error={hasErrors ? formErrors.newPassword : null}
-       />
-
-       <Input
-         label='Repeat Password'
-         placeholder='Repeat Password'
-         name='repeatPassword'
-         type='password'
-         onChange={this.handleChange}
-         value={this.state.repeatPassword}
-         error={hasErrors ? formErrors.repeatPassword : null}
-       />
-
-       <Button name='Confirm New Password'  type='submit'/>
-     </form>
-     </Wrapper>
-   );
- }
+          <Button name="Confirm New Password" type="submit" />
+        </form>
+      </Wrapper>
+    );
+  }
 }
 
 export default PasswordForm;
--- client/src/components/ProfilePage/ProfileInfo.js
+++ client/src/components/ProfilePage/ProfileInfo.js
@@ -3,25 +3,27 @@ import makeRequest from '../../utils/makeRequest';
 import { UsersDB } from '../../dummyDB';
 
 export default class ProfileInfo extends Component {
-    state = {
-        user: UsersDB
-    }
+  state = {
+    user: UsersDB
+  };
 
-    componentDidMount() {
-        makeRequest('/api/users', 'GET') //substitute userID with the real one
-            .then(res => res.json())
-            .then(res => {
-                console.log('>> ProfilePage/ProfileInfo GET res,', res)
-            })
-            .catch(err => err)
-    }
-    render() {
-        return (
-            <div className='profileInfo'>
-                <p>Name: {this.state.user.name}</p>
-                <p>Email: {this.state.user.email}</p>
-                <p>Phone: {this.state.user.phone}</p>
-            </div>
-        )
-    }
+  componentDidMount() {
+    //substitute userID with the real one
+    makeRequest('/api/users', 'GET')
+      .then(res => res.json())
+      .then(res => console.log(`ProfileInfo: ${res}`))
+      .catch(err => err);
+  }
+
+  render() {
+    const { name, email, phone } = this.state;
+
+    return (
+      <div className="profileInfo">
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phone}</p>
+      </div>
+    );
+  }
 }
