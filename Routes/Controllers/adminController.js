304863522166942479055318616e860794bfc80b
David De Wulf <contact.dewulf@gmail.com>
2019-02-11 17:04:40 -0800

    Resolve npm conflict

--- Routes/Controllers/adminController.js
+++ Routes/Controllers/adminController.js
@@ -5,58 +5,62 @@ const path = require('path');
 // const config = require('../../config');
 const { respond } = require('../../lib');
 
-function login (req, res, next) {    
-    const isValid = true;
-    
-	if (isValid) {
-		console.log('[login.req.body]', req.body)
-		
-		let user = db.User.findOne({ email });
-		// Match password i
-		// let isMatch = await user.validatePassword(password);
-		isMatch = true;
-		console.log('[login] login is forced to', isMatch);
-		const token = user.generateJWT();
-		console.log('[login] token', token);
-		return respond(res, true, { token })
+async function login(req, res, next) {
+  const isValid = true;
 
-        // const payload = passport.authenticate(
-        //     'local', 
-        //     (err, passport, info) => {
-        //         console.log('[login.passport.authenticate]', err, passport.user, info);
-		// 		if (err) return next(err);
+  if (isValid) {
+    console.log('[login.req.body]', req.body);
 
-		// 		if (passport) {
-		// 			const user = passport.user;
-		// 			user.generateJWT();
-		// 			return respond(res, true, { user: user.toAuthJSON() });
-		// 		}
+    let user = await db.User.findOne({ email });
+    console.log('TCL: login -> user', user);
 
-		// 		if (info) return respond(res, false, info);
+    return user;
+    // console.log('TCL: login -> user', user)
+    // Match password i
+    // let isMatch = await user.validatePassword(password);
+    // isMatch = true;
+    // console.log('[login] login is forced to', isMatch);
+    // const token = user.generateJWT();
+    // console.log('[login] token', token);
+    // return respond(res, true, { token })
 
-		// 		return respond(res, false)
-		// 	}
-        // )(req, res, next);
+    // const payload = passport.authenticate(
+    //     'local',
+    //     (err, passport, info) => {
+    //         console.log('[login.passport.authenticate]', err, passport.user, info);
+    // 		if (err) return next(err);
 
-        // console.log('[login] payload,', payload)
-        
-        // return payload;
-	}
+    // 		if (passport) {
+    // 			const user = passport.user;
+    // 			user.generateJWT();
+    // 			return respond(res, true, { user: user.toAuthJSON() });
+    // 		}
+
+    // 		if (info) return respond(res, false, info);
+
+    // 		return respond(res, false)
+    // 	}
+    // )(req, res, next);
+
+    // console.log('[login] payload,', payload)
+
+    // return payload;
+  }
 }
 
 function loadReactRouter(req, res) {
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 }
 
 // Session Handling
 function requireAdminLogin(req, res, next) {
-	if (req.session.admin) next();
-	else res.redirect('/login');
+  if (req.session.admin) next();
+  else res.redirect('/login');
 }
 
 function requireUserLogin(req, res, next) {
-	if (req.session.username) next();
-	else res.redirect('/login');
+  if (req.session.username) next();
+  else res.redirect('/login');
 }
 
 // 	passport.authenticate('jwt', {
@@ -69,28 +73,27 @@ function requireUserLogin(req, res, next) {
 // 	}
 
 function ensureAuthenticated(req, res, next) {
-	
-	if (req.isAuthenticated()) {
-		return next(null);
-	}
-	res.redirect('/error');
+  if (req.isAuthenticated()) {
+    return next(null);
+  }
+  res.redirect('/error');
 }
 
 // Logout current user
 function logout(req, res) {
-	req.session.destroy(() => {
-		console.log('User signed out.');
-	});
-	req.logout();
-	req.flash('success_msg', 'You are logged out');
-	res.redirect('/login');	
+  req.session.destroy(() => {
+    console.log('User signed out.');
+  });
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
 }
 
 module.exports = {
-	login,
-	loadReactRouter,
-	ensureAuthenticated,
-	requireUserLogin,
-	requireAdminLogin,
-	logout
+  login,
+  loadReactRouter,
+  ensureAuthenticated,
+  requireUserLogin,
+  requireAdminLogin,
+  logout
 };
