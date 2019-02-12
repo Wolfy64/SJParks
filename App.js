a2b8b4709ac5ba90770223c901e3719114caefa6
David De Wulf <contact.dewulf@gmail.com>
2019-02-12 11:49:05 -0800

    Add JWT Authentication

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ require('dotenv-safe').load();
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const addRequestId = require('express-request-id')();
 const formData = require('express-form-data');
 /** Load Configurations */
@@ -19,48 +21,70 @@ app.use(express.static(path.join(__dirname, config.keys.path)));
 app.use(addRequestId);
 logger.token('id', req => req.sessionID.split('-')[0]);
 app.use(logger('combined' /*, {skip: (req, res) => res.statusCode < 400 }*/));
-app.use(logger('> [:date[iso]] :method :url :status :response-time ms - :res[content-length] >'));
+app.use(
+  logger(
+    '> [:date[iso]] :method :url :status :response-time ms - :res[content-length] >'
+  )
+);
 
 /** Parser */
 app.use(formData.parse());
 app.use(express.json());
 app.use(
-	express.urlencoded({
-		extended: true
-	})
+  express.urlencoded({
+    extended: true
+  })
 );
 
 /** Passport */
 // config.passport(app);
 
-/* JWT Authentication */
-//app.use()
+/** Use Express middleware to handle cookies (JWT) */
+app.use(cookieParser());
+
+/** JWT Authentication */
+app.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    console.log('TCL: token', token);
+    const user = jwt.verify(token, config.keys.secret);
+    res.json({ user });
+  }
+
+  next();
+});
 
 /** Routes */
 app.use(function(req, res, next) {
-	console.log('request', req.url, req.body, req.method);
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-token');
-	if (req.method === 'OPTIONS') {
-		res.end();
-	} else {
-		next();
-	}
+  console.log('request', req.url, req.body, req.method);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-token'
+  );
+  if (req.method === 'OPTIONS') {
+    res.end();
+  } else {
+    next();
+  }
 });
 
 app.use('/api', router.api);
 
 // router.all('/api/*', ensureAuthenticated);
 app.post('/login', router.auth);
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, config.keys.path, 'index.html')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, config.keys.path, 'index.html'))
+);
 
 /** Error Handlers */
 app.use((err, req, res, next) => {
-	res.status(500).json({
-		errors: {
-			message: err
-		}
-	});
+  res.status(500).json({
+    errors: {
+      message: err
+    }
+  });
 });
 
 module.exports = app;
