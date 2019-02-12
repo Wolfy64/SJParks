0da37be44fc16f83e24fb4e332b476cc3c078480
Irina Mityugova <irishka2863@yahoo.com>
2019-02-12 14:29:53 -0800

    Mobile CSS

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -5,6 +5,8 @@ import SideBar from '../SideBar';
 
 const Page = styled.div`
   margin: 0 80px 0 240px;
+  display: flex;
+  flex-wrap: wrap;
   z-index: 0;
   @media screen and (max-width: ${(props) => props.theme.displays.tablet}) {
     margin: 100px 0;
