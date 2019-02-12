ced4954b9bb0d18bb3e083f72ca8f7d6a859bada
Irina Mityugova <irishka2863@yahoo.com>
2019-02-12 14:00:39 -0800

    Mobile CSS

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -6,8 +6,8 @@ import SideBar from '../SideBar';
 const Page = styled.div`
   margin: 0 80px 0 240px;
   z-index: 0;
-  @media screen and (max-width: ${(props) => props.theme.displays.mobileL}) {
-    margin: 30px 0;
+  @media screen and (max-width: ${(props) => props.theme.displays.tablet}) {
+    margin: 100px 0;
   }
 `;
 
