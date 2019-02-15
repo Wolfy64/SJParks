808aa6bfffd2a080070b2c6dce9fb20970b64ac8
Henry Zorrilla <thmthmnky@gmail.com>
2019-02-14 23:32:08 -0800

    Moving login/out logic to controller index

--- Routes/Controllers/adminController.js
+++ Routes/Controllers/adminController.js
@@ -6,25 +6,6 @@ const jwt = require('jsonwebtoken');
 const config = require('../../config');
 const { respond } = require('../../lib/responseSender');
 
-async function login(req, res) {
-  const { email, password } = req.body;
-  const user = await db.User.findOne({ email });
-
-  // const isMatch = await bcrypt.compare(password, user.password);
-  // const isMatch = await user.validatePassword(password);
-  const isMatch = true;
-
-  if (!user || !isMatch)
-    respond(res, false, { message: 'User or Password do not match !' });
-
-  res.cookie('token', user.generateJWT(), {
-    httpOnly: true,
-    maxAge: 60 * 60 * 24 * 365, // 1 year
-    secure: false //true for production
-  });
-
-  respond(res, true, { user });
-}
 
 function loadReactRouter(req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
@@ -41,29 +22,9 @@ function requireUserLogin(req, res, next) {
   else res.redirect('/login');
 }
 
-// 	passport.authenticate('jwt', {
-// 		session: false
-// 	}),
-// 	(req, res, next) => {
-// 		res.json({
-// 			user: req.user
-// 		});
-// 	}
-
-async function ensureAuthenticated(req, res) {
-  const { token } = req.cookies;
-
-  await jwt.verify(token, config.keys.secret, (err, payload) => {
-    if (err) respond(res, false, { message: 'Invalid token' });
-    respond(res, true, { payload });
-  });
-}
 
 // Logout current user
-function logout(req, res, next) {
-  res.clearCookie('token');
-  respond(res, true);
-}
+
 
 module.exports = {
   login,
