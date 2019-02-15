b16af544e5d2e256fdcad575fbea499c9f7da70b
David De Wulf <contact.dewulf@gmail.com>
2019-02-14 16:17:12 -0800

    If no route Redirect to /

--- client/src/components/Routes/PublicRoutes.js
+++ client/src/components/Routes/PublicRoutes.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import PublicPage from '../PublicPage';
 import Login from '../LoginPage';
-import NoMatch from '../UI/NoMatch';
 
 const PublicRoutes = () => (
   <Switch>
     <Route path="/" component={PublicPage} exact />
     <Route path="/login" component={Login} />
-    <Route component={NoMatch} />
+    <Route component={() => <Redirect to="/" />} />
   </Switch>
 );
 
