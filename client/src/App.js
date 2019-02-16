0ecf3496e64a26b14a08b6f959a7daa33e481f47
David De Wulf <contact.dewulf@gmail.com>
2019-02-15 18:05:48 -0800

    Handle JWT Expiration

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { withRouter } from "react-router";
-import PublicRoutes from "./components/Routes/PublicRoutes";
-import ProtectedRoutes from "./components/Routes/ProtectedRoutes";
-import makeRequest from "./utils/makeRequest";
+import React from 'react';
+import { withRouter } from 'react-router';
+import PublicRoutes from './components/Routes/PublicRoutes';
+import ProtectedRoutes from './components/Routes/ProtectedRoutes';
+import makeRequest from './utils/makeRequest';
 
 class App extends React.Component {
   state = { isAuthenticated: false };
 
   async componentDidMount() {
-    const request = await makeRequest("/api/auth");
+    const request = await makeRequest('/api/auth');
     const { success, user } = await request.json();
 
     this.setState({ isAuthenticated: success, user });
