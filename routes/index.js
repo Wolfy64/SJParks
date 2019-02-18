9c54fed29d9ea1b6bbef44d31c6a261bdc3bb727
Henry Zorrilla <thmthmnky@gmail.com>
2019-02-17 16:43:26 -0800

    pulling from upstream master

--- routes/index.js
+++ routes/index.js
@@ -1,73 +1,24 @@
 /*jshint esversion: 8 */
-const express = require('express');
+// import Users from '../client/src/components/UsersPage/index';
 const db = require('../models');
-const controllers = require('./controllers');
-const allRouters = [];
-module.exports.create = () => {
-	for (const Model in db) {
-		var newRouter = express.Router();
-		var modelArray = Model.split('');
-		var newFirstLetter = modelArray.shift();
-		newFirstLetter = newFirstLetter.toLowerCase();
-		modelArray.unshift(newFirstLetter);
-		const model = modelArray.reduce((acc, curr) => acc + curr);
+var apiRouter = require('express').Router();
+const {
+  users,
+  parks,
+  updates,
+  subscriptionLogs,
+  messageLogs,
+} = require('./controllers');
 
-    // console.log(`Creating the ${model}s router using: create${Model}Router`);
-    
-    // console.log(controllers[`${model}s`]);
-    const router = controllers[`${model}s`];
-    // console.log(typeof (router));
+apiRouter.param('userId', async (req, res, next, userId) => { 
+  req.user = await db.User.findById(userId);
+  return next();
+});
 
-		newRouter.use(`${model}s`, () => router);
-		allRouters.push(newRouter);
-	}
-	// console.log(allRouters);
-	return allRouters;
-};
+apiRouter.use('/users' , users);
+apiRouter.use('/parks' , parks);
+apiRouter.use('/updates' , updates);
+apiRouter.use('/subscriptionLogs' , subscriptionLogs);
+apiRouter.use('/messageLogs' , messageLogs);
 
-/**
- * module.exports.dbSeedEngine = (Lists) => {
-  const db = require('../models');
-  var seeder = [
-    [], {}
-  ];
-  
-  const {UserList, ParkList, MessageList, MessageLog, SubscriptionLog} = Lists;
-  
-  if (!(UserList || ParkList || MessageList || MessageLog || SubscriptionLog))
-  {
-    console.error(" A list of some sort is required...");
-  } else
-  {
-
-   for(const Model in db) {
-      seeder[0].push(Model);
-    }
-
-    seeder[1].userList = UserList;
-    seeder[1].parkList = ParkList;
-    seeder[1].messageList = MessageList;
-    seeder[1].messageLog = MessageLog;
-    seeder[1].subscriptionLog = SubscriptionLog;
-
-    console.log(seeder[1]);
-
-    // seed the entire db
-    seeder[0].forEach(Schema =>
-    {
-      Schema.remove({})
-        .then(elements => { console.log(`${Schema} has been wiped clean.`); })
-        .catch(err => console.error(err));
-
-      Schema.create(seeder[1].pop(seeder[0].indexof(Schema)))
-        .then(elements =>
-        {
-          console.log(`${Schema}'s have been successfully seeded. Exiting process...`);
-        })
-        .catch(err => console.log(err));
-    });
-    process.exit();
-  }
-};
-
-* */
+module.exports = apiRouter;
--- server.js
+++ server.js
@@ -1,130 +1,25 @@
 /*jshint esversion: 8 */
-/** Load Dependencies */
-const http = require('http');
-
 /** Load Configurations */
 require('dotenv-safe').load();
-const webapp = require('./app');
-const config = require('./config');
+const http = require('http');
+const app = require('./app');
+const config = require('./configurations');
+const mongoose = require('mongoose',{ useMongoClient: true});
 console.log(`> Running Server in ${process.env.NODE_ENV} mode...`);
 
-/** Configure Mongoose */
-const mongoose = require('mongoose',{
-  useMongoClient: true
-});
-mongoose.set('debug', true);
+/** Configure Mongoose Database */
 mongoose.Promise = global.Promise;
-
-const opts = {
+mongoose.set('debug', true);
+mongoose
+  .connect(config.keys.url, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
-};
-
-/** Connect Mongoose to MongoDB */
-mongoose.connect(config.keys.url, opts)
-  .then(() => console.log(`> MongoDB connected...`))
+} )
+  .then(() => { console.log(`> MongoDB connected...`);})
   .catch(err => console.error(err));
 
-/** Create Express-App Server */
-const server = http.createServer(webapp);
-
-/** Deploy Express-App to Local-Host*/
+/** Deploy Express Server*/
+const server = http.createServer(app);
 server.listen(config.keys.port, () => console.log(`> Express Server Deployed @url: http://localhost:${config.keys.port}...`));
 
-
-/*
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var db = require('./db');
-
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-
-
-
-// Create a new Express application.
-var app = express();
-
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
-
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
-  
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-  
-app.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
-
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
-
-app.listen(3000);
-*/
