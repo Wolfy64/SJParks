8dcaa393138608c6f871eaed953b64f7a2ed8e7d
David De Wulf <contact.dewulf@gmail.com>
2019-02-25 16:49:15 -0800

    NavBar

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Container } from './styles';
 import UserLink from '../UserLink';
-import SideBar from '../SideBar';
+import NavBar from '../NavBar';
 
 import ProtectedRoutes from '../Routes/ProtectedRoutes';
 
 const Dashboard = ({ user }) => (
   <>
     <UserLink user={user} />
-    {/* <TopBar user={user} /> */}
-    <SideBar user={user} />
+    <NavBar user={user} />
     <Container>
       <ProtectedRoutes user={user} />
     </Container>
