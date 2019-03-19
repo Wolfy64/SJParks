e258dc848bac08ba403163525b6a4729e99ebcde
David De Wulf <ledavid64@gmail.com>
2019-03-18 18:55:47 -0700

    :bug: Fix keys path

--- models/User.js
+++ models/User.js
@@ -8,111 +8,112 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema(
-	{
-		active: {
-			type: Boolean,
-			default: true
-		},
+  {
+    active: {
+      type: Boolean,
+      default: true
+    },
 
-		access: {
-			type: String,
-			default: 'basic'
-		},
+    access: {
+      type: String,
+      default: 'basic'
+    },
 
-		salt: String,
+    salt: String,
 
-		imageUrl: String,
+    imageUrl: String,
 
-		firstName: String,
+    firstName: String,
 
-		lastName: String,
+    lastName: String,
 
-		email: {
-			index: true,
-			type: String,
-			required: [true, 'you must enter an email']
-		},
+    email: {
+      index: true,
+      type: String,
+      required: [true, 'you must enter an email']
+    },
 
-		phone: {
-			index: true,
-			type: String,
-			required: [true, 'you must enter a phone number']
-		},
+    phone: {
+      index: true,
+      type: String,
+      required: [true, 'you must enter a phone number']
+    },
 
-		password: {
-			type: String,
-			required: [true, 'you must provide a password']
-		},
+    password: {
+      type: String,
+      required: [true, 'you must provide a password']
+    },
 
-		parks: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Park'
-			}
-		],
+    parks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Park'
+      }
+    ],
 
-		updates: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Update'
-			}
-		]
-	},
-	{
-		timestamps: true
-	}
+    updates: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Update'
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
 );
 
 // Create Schema Virtuals
 UserSchema.virtual('name').set(function(props) {
-	const N = props.split(' ');
-	this.firstName = N[0];
-	this.lastName = N[1];
+  const N = props.split(' ');
+  this.firstName = N[0];
+  this.lastName = N[1];
 });
 
 UserSchema.virtual('name').get(function() {
-	return this.firstName + ' ' + this.lastName;
+  return this.firstName + ' ' + this.lastName;
 });
 
 UserSchema.virtual('subscription').set(function(props) {
-	const { access, active, email, twilio } = props;
-	this.active = active;
-	this.access = access;
-	this.email = email;
-	if (twilio.phone) this.twilio.phone = twilio.phone;
-	if (twilio.subscriptions) this.twilio.subscriptions.push(twilio.subscriptions);
+  const { access, active, email, twilio } = props;
+  this.active = active;
+  this.access = access;
+  this.email = email;
+  if (twilio.phone) this.twilio.phone = twilio.phone;
+  if (twilio.subscriptions)
+    this.twilio.subscriptions.push(twilio.subscriptions);
 });
 
 UserSchema.virtual('subscription').get(function() {
-	return {
-		active: this.active,
-		access: this.access,
-		email: this.email,
-		twilio: {
-			phone: this.phone,
-			subscriptions: this.parks
-		}
-	};
+  return {
+    active: this.active,
+    access: this.access,
+    email: this.email,
+    twilio: {
+      phone: this.phone,
+      subscriptions: this.parks
+    }
+  };
 });
 
 UserSchema.set('toJSON', { getters: true, virtuals: true });
 
-/** Configure Custom Validators */ 
+/** Configure Custom Validators */
 UserSchema.plugin(uniqueValidator, {
-	type: 'mongoose-unique-validator'
+  type: 'mongoose-unique-validator'
 });
 
-UserSchema.pre('save', async function (next) {
-	console.log(`Entered 'pre' save hook`);
-	var user = this;
-	if (!user.isModified('password')) return next();
-	await user.setPassword(user.password);
-	next();
+UserSchema.pre('save', async function(next) {
+  console.log(`Entered 'pre' save hook`);
+  var user = this;
+  if (!user.isModified('password')) return next();
+  await user.setPassword(user.password);
+  next();
 });
 
 /**
  * Schema Methods
- * 
+ *
  * @property
       " Do not declare methods using ES6 arrow functions (=>). Arrow functions explicitly prevent binding this, so your method will not have access to the document and the above *examples will not work."
  *
@@ -121,68 +122,68 @@ UserSchema.pre('save', async function (next) {
 
 /**
  * Set user password
- * 
+ *
  * @param {string} newPassword
- * 
+ *
  */
 UserSchema.methods.setPassword = async function setPassword(newPassword) {
-	console.log(`[User Model] setting password`);
-	var salt = await bcrypt.genSaltSync(16);
-	this.salt = salt;
+  console.log(`[User Model] setting password`);
+  var salt = await bcrypt.genSaltSync(16);
+  this.salt = salt;
 
-	var password = await bcrypt.hashSync(newPassword, this.salt);
-	this.password = password;
-	console.log(`Pasword has been set to ${this.password}`);
+  var password = await bcrypt.hashSync(newPassword, this.salt);
+  this.password = password;
+  console.log(`Pasword has been set to ${this.password}`);
 };
 
 /**
  * Validate user  password
- * 
+ *
  * @param {string} candidatePassword
- * 
+ *
  */
 UserSchema.methods.validatePassword = async function(candidatePassword) {
-	return new Promise(async (resolve, reject) => {
-		await bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-			if (err) return reject(err);
-			resolve(isMatch);
-		});
-	});
+  return new Promise(async (resolve, reject) => {
+    await bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) return reject(err);
+      resolve(isMatch);
+    });
+  });
 };
 
 /**
  * Generate web token
- * 
+ *
  * @returns {jwt} token
- * 
+ *
  */
 UserSchema.methods.generateJWT = function() {
-	const today = new Date();
-	const expirationDate = new Date(today);
-	expirationDate.setDate(today.getDate() + 30);
-	const token = jwt.sign(
-		{
-			_id: this._id,
-			userName: this.userName,
-			access: this.access,
-			expirationDate: parseInt(expirationDate.getTime() / 1000, 10)
-		},
-		require('../config/keys').secret,
-		{ expiresIn: '30 days' }
-	);
-	return token;
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 30);
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      userName: this.userName,
+      access: this.access,
+      expirationDate: parseInt(expirationDate.getTime() / 1000, 10)
+    },
+    require('../configurations/keys').secret,
+    { expiresIn: '30 days' }
+  );
+  return token;
 };
 
 /**
- * 
+ *
  */
 UserSchema.methods.toAuthJSON = function() {
-	return {
-		_id: this._id,
-		name: this.name,
-		email: this.email,
-		token: this.generateJWT()
-	};
+  return {
+    _id: this._id,
+    name: this.name,
+    email: this.email,
+    token: this.generateJWT()
+  };
 };
 
 module.exports = mongoose.model('User', UserSchema);
