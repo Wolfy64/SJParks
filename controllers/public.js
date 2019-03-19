168aec9cdce790f6f2b8c258e34065881011adbc
David De Wulf <ledavid64@gmail.com>
2019-03-19 01:17:30 -0700

    ✨ Add auth route

--- controllers/public.js
+++ controllers/public.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const jwt = require('jsonwebtoken');
 const db = require('../models');
 const { respond } = require('../lib');
 const keys = require('../configurations/keys');
@@ -7,10 +8,19 @@ const home = (req, res) => {
   res.sendFile(path.join(`${__dirname}/../`, keys.path, 'index.html'));
 };
 
+const auth = async (req, res) => {
+  const { token } = req.cookies;
+
+  await jwt.verify(token, keys.secret, (err, payload) => {
+    if (err) return respond(res, false, null, 'Invalid token');
+    return respond(res, true, payload);
+  });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await db.User.findOne({ email });
-  const isMatch = await user.validatePassword(password, user.password);
+  const isMatch = await user.validatePassword(password);
 
   if (user && isMatch) {
     res.cookie('token', user.generateJWT(), {
@@ -26,6 +36,7 @@ const login = async (req, res) => {
 };
 
 module.exports = {
+  auth,
   home,
   login
 };
--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { public } = require('../controllers');
 const router = express.Router();
 
+router.get('/auth', public.auth);
 router.post('/login', public.login);
 router.use(public.home);
 
