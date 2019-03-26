ef7a63da8c81471b19737c486a130a48d59bcb7b
David De Wulf <ledavid64@gmail.com>
2019-03-26 16:51:59 -0700

    🚚 Move styles to styles.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,22 @@
 /*jshint esversion: 8 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import App from './App';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
-// import * as serviceWorker from './serviceWorker';
-
-const Container = styled.div`
- max-width: ${props => props.theme.displays.hd};
-
- @media screen and (min-width: ${props => props.theme.displays.hd}) {
-   border: 1px solid ${props => props.theme.colors.dark};
- }
-
- @media screen and (max-width: ${props => props.theme.displays.mobileL}) {
-   display: inline-block;
- }
-`;
+import { Container } from './styles';
 
 const app = (
- <BrowserRouter>
-   <ThemeProvider theme={theme}>
-     <Container>
-       <GlobalStyle />
-       <App />
-     </Container>
-   </ThemeProvider>
- </BrowserRouter>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <GlobalStyle />
+        <App />
+      </Container>
+    </ThemeProvider>
+  </BrowserRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-
