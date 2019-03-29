6b0a72987537e44ce52205fc45b328dcdb9c63a1
David De Wulf <ledavid64@gmail.com>
2019-03-28 19:00:21 -0700

    :white_check_mark: Add simple test

--- client/src/__test__/App.test.js
+++ client/src/__test__/App.test.js
@@ -1,13 +1,11 @@
 /*jshint esversion: 8 */
 import React from 'react';
 import { withRouter } from 'react-router';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from '../App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(withRouter(<App />), div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(withRouter(<App />));
   });
 });
