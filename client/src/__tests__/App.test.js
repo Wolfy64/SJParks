1c7632fb72a83a9f6035613c120e7d4993a54947
David De Wulf <ledavid64@gmail.com>
2019-03-29 01:34:28 -0700

    ✅Add smoke test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,11 +1,14 @@
-/*jshint esversion: 8 */
 import React from 'react';
-import { withRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-testing-library';
 import App from '../App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
-    render(withRouter(<App />));
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
   });
 });
